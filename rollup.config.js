4d96d5265bec8b7fee508eaf087dce7a203aefde
Richard Davey <rich@photonstorm.com>
2020-04-15 16:22:41 +0100

    Added debug script

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 // Can't use official plugin because: https://github.com/rollup/plugins/issues/287 >:-(
 // import typescript from '@rollup/plugin-typescript';
+
 import clear from 'rollup-plugin-clear';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
@@ -8,6 +9,7 @@ import filesize from 'rollup-plugin-filesize';
 import command from 'rollup-plugin-command';
 import fs from 'fs-extra';
 import copy from 'rollup-plugin-copy';
+
 // Another crashing plugin that used to work (and hopefully will again!):
 // import visualizer from 'rollup-plugin-visualizer';
 
@@ -101,7 +103,7 @@ export default {
 
         copy({
             targets: [
-                { src: './README.dist.md', dest: 'dist', rename: 'README.md' }
+                { src: 'README.dist.md', dest: 'dist', rename: 'README.md', copyOnce: true }
             ]
         }),
 
