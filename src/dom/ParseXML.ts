4f70eada716a4c176ba970d055809810ebe545d2
Richard Davey <rich@photonstorm.com>
2020-04-16 14:24:39 +0100

    Moved ParseXML into the dom folder

---
+++ src/dom/ParseXML.ts
@@ -0,0 +1,41 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2020 Photon Storm Ltd.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+/**
+ * Takes the given data string and parses it as XML using the native DOMParser interface.
+ * The parsed XML object is returned, or `null` if there was an error while parsing the data.
+ *
+ * @function Phaser.DOM.ParseXML
+ * @since 3.0.0
+ *
+ * @param {string} data - The XML source stored in a string.
+ *
+ * @return {?XMLDocument} The parsed XML data, or `null` if the data could not be parsed.
+ */
+export default function ParseXML (data: string): XMLDocument | null
+{
+    let xml: XMLDocument;
+
+    try 
+    {
+        const parser = new DOMParser();
+
+        xml = parser.parseFromString(data, 'text/xml');
+
+        if (!xml || !xml.documentElement || xml.getElementsByTagName('parsererror').length)
+        {
+            return null;
+        }
+        else
+        {
+            return xml;
+        }
+    }
+    catch (error)
+    {
+        return null;
+    }
+}
--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,6 +1,7 @@
 import AddToDOM from './AddToDOM';
 import DOMContentLoaded from './DOMContentLoaded';
 import GetElement from './GetElement';
+import ParseXML from './ParseXML';
 import RemoveFromDOM from './RemoveFromDOM';
 
 //  @namespace Phaser.DOM
@@ -9,5 +10,6 @@ export {
     AddToDOM,
     DOMContentLoaded,
     GetElement,
+    ParseXML,
     RemoveFromDOM
 }
