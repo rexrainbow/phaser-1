24a973ca818edbe5964ed05d2c7383a8a4b6cfc8
Richard Davey <rich@photonstorm.com>
2020-04-16 17:58:01 +0100

    Expose Line

--- src/geom/index.ts
+++ src/geom/index.ts
@@ -1,9 +1,11 @@
 import * as Circle from './circle';
 import * as Ellipse from './ellipse';
+import * as Line from './line';
 import * as Rectangle from './rectangle';
 
 export default {
     Circle,
     Ellipse,
+    Line,
     Rectangle
 }
