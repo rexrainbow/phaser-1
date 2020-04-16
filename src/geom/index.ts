78e1a50df96ba01ce558f3eed95c8cb3e686c6c9
Richard Davey <rich@photonstorm.com>
2020-04-16 22:30:44 +0100

    Expose Triangle

--- src/geom/index.ts
+++ src/geom/index.ts
@@ -2,10 +2,12 @@ import * as Circle from './circle';
 import * as Ellipse from './ellipse';
 import * as Line from './line';
 import * as Rectangle from './rectangle';
+import * as Triangle from './triangle';
 
 export default {
     Circle,
     Ellipse,
     Line,
-    Rectangle
+    Rectangle,
+    Triangle
 }
