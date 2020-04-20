0cb8d7ee2670ed8fa3c806b1f65c8de704111d08
Samuel Batista <sambatista1@gmail.com>
2020-04-19 23:32:15 -0400

    Remove IDestroyable unnecessary interface.

--- src/types/IDestroyable.ts
+++
@@ -1,5 +0,0 @@
-import IParent from "../gameobjects/container/IParent";
-
-export interface IDestroyable {
-	destroy: (reparentChildren?: IParent) => void;
-}
--- src/world/IWorld.ts
+++ src/world/IWorld.ts
@@ -1,9 +1,8 @@
 import ICamera from '../camera/ICamera';
 import IGameObject from '../gameobjects/gameobject/IGameObject';
 import ISprite from '../gameobjects/sprite/ISprite';
-import { IDestroyable } from '../types/IDestroyable';
 
-export default interface IWorld extends IDestroyable
+export default interface IWorld
 {
     camera: ICamera;
     dirtyFrame: number;
@@ -15,4 +14,5 @@ export default interface IWorld extends IDestroyable
     update (delta?: number, time?: number): void;
     render (gameFrame: number): number;
     shutdown (): void;
+    destroy(): void;
 }
