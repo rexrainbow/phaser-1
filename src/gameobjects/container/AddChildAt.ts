977737c61262db2a471340e20da02acb69ab2827
Richard Davey <rich@photonstorm.com>
2020-04-21 18:02:04 +0100

    Tidying up

--- src/gameobjects/container/AddChildAt.ts
+++ src/gameobjects/container/AddChildAt.ts
@@ -2,20 +2,20 @@ import { IGameObject } from '../gameobject/IGameObject';
 import { IParent } from './IParent';
 import { SetParent } from './SetParent';
 
-export function AddChildAt (parent: IParent, index: number, ...child: IGameObject[])
+export function AddChildAt (parent: IParent, index: number, ...child: IGameObject[]): void
 {
     const children = parent.children;
 
     if (index >= 0 && index <= children.length)
     {
-        child.reverse().forEach(entity => {
-
+        child.reverse().forEach(entity =>
+        {
             SetParent(parent, entity);
 
             children.splice(index, 0, entity);
 
             entity.updateTransform();
-    
+
         });
     }
 }
--- src/gameobjects/container/AddChildren.ts
+++ src/gameobjects/container/AddChildren.ts
@@ -2,15 +2,16 @@ import { IGameObject } from '../gameobject/IGameObject';
 import { IParent } from './IParent';
 import { SetParent } from './SetParent';
 
-export function AddChildren (parent: IParent, ...children: IGameObject[])
+export function AddChildren (parent: IParent, ...children: IGameObject[]): IGameObject[]
 {
-    children.forEach(child => {
-
+    children.forEach(child =>
+    {
         SetParent(parent, child);
 
         parent.children.push(child);
 
         child.updateTransform();
-
     });
+
+    return children;
 }
