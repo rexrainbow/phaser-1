7662fbb3a3edad6a9d2818698c332c02d3369464
Richard Davey <rich@photonstorm.com>
2020-04-22 17:56:23 +0100

    Create CreateSceneRenderData.ts

---
+++ src/scenes/CreateSceneRenderData.ts
@@ -0,0 +1,13 @@
+import { ISceneRenderData } from './ISceneRenderData';
+
+export function CreateSceneRenderData (): ISceneRenderData
+{
+    return {
+        gameFrame: 0,
+        numTotalFrames: 0,
+        numDirtyFrames: 0,
+        numDirtyCameras: 0,
+        numRenderedWorlds: 0,
+        renderedWorlds: []
+    };
+}
