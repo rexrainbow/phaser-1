c73d5d80dd4b3d33f92b2b0108bcc01854895ab8
Richard Davey <rich@photonstorm.com>
2020-04-23 14:54:17 +0100

    Update and rename canPlayH264Video.ts to CanPlayH264Video.ts

--- src/device/video/canPlayH264Video.ts
+++
@@ -1,4 +0,0 @@
-export function canPlayH264Video (videoElement: HTMLVideoElement = document.createElement('video')): boolean
-{
-    return (videoElement.canPlayType('video/mp4; codecs="avc1.42E01E"') !== '');
-}
