77e96e56029f656dde88dc42b52e52db4a003a63
Richard Davey <rich@photonstorm.com>
2020-04-23 14:55:23 +0100

    Update and rename canPlayWebMVideo.ts to CanPlayWebMVideo.ts

--- src/device/video/canPlayWebMVideo.ts
+++
@@ -1,4 +0,0 @@
-export function canPlayWebMVideo (videoElement: HTMLVideoElement = document.createElement('video')): boolean
-{
-    return (videoElement.canPlayType('video/webm; codecs="vp8, vorbis"') !== '');
-}
