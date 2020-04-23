2d4e225824dd538650ffdb6c2b93d590ca5d8271
Richard Davey <rich@photonstorm.com>
2020-04-23 14:47:29 +0100

    Update IsSafari.ts

--- src/device/browser/IsSafari.ts
+++ src/device/browser/IsSafari.ts
@@ -4,7 +4,7 @@ export function IsSafari (): { safari: boolean; safariVersion: number }
 {
     const ua: string = navigator.userAgent;
 
-    const safari = (ua.includes('Safari') && !isWindowsPhone());
+    const safari = (ua.includes('Safari') && !IsWindowsPhone());
     const safariVersion = ((/Version\/(\d+)\./).test(ua)) ? parseInt(RegExp.$1, 10) : 0;
 
     return {
