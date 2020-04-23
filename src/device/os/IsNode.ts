9d770364fdcb25beac8059dff36b1ec05850a1d2
Richard Davey <rich@photonstorm.com>
2020-04-23 14:52:09 +0100

    Update and rename isNode.ts to IsNode.ts

--- src/device/os/IsNode.ts
+++ src/device/os/IsNode.ts
@@ -1,4 +1,4 @@
-export function isNode (): boolean
+export function IsNode (): boolean
 {
     return (typeof process !== 'undefined' && typeof process.versions === 'object' && process.versions.hasOwnProperty('node'));
 }
