3be35ff89978c3fb5b51933410a035260c8bbb62
Richard Davey <rich@photonstorm.com>
2020-04-23 14:51:12 +0100

    Update and rename isKindle.ts to IsKindle.ts

--- src/device/os/IsKindle.ts
+++ src/device/os/IsKindle.ts
@@ -1,4 +1,4 @@
-export function isKindle (): boolean
+export function IsKindle (): boolean
 {
     // This will NOT detect early generations of Kindle Fire, I think there is no reliable way...
     // E.g. "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.1.0-80) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true"
