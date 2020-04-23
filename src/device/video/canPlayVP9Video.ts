5a1f9ec17b293ac9e860834be18fb64c6205de7e
Richard Davey <rich@photonstorm.com>
2020-04-23 14:55:04 +0100

    Update and rename canPlayVP9Video.ts to CanPlayVP9Video.ts

--- src/device/video/canPlayVP9Video.ts
+++
@@ -1,4 +0,0 @@
-export function canPlayVP9Video (videoElement: HTMLVideoElement = document.createElement('video')): boolean
-{
-    return (videoElement.canPlayType('video/webm; codecs="vp9"') !== '');
-}
