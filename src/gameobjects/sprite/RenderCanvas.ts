caad8149bc8f447a31d4d0b1afae7c6ff3cdc2e9
Richard Davey <rich@photonstorm.com>
2020-04-24 18:02:00 +0100

    Create RenderCanvas.ts

---
+++ src/gameobjects/sprite/RenderCanvas.ts
@@ -0,0 +1,18 @@
+import { ISprite } from './ISprite';
+
+export function RenderCanvas (sprite: ISprite, ctx: CanvasRenderingContext2D): void
+{
+    const frame = sprite.frame;
+
+    const { a, b, c, d, tx, ty } = sprite.worldTransform;
+
+    ctx.save();
+
+    ctx.setTransform(a, b, c, d, tx, ty);
+
+    ctx.globalAlpha = sprite.alpha;
+
+    ctx.drawImage(frame.texture.image as HTMLImageElement, frame.x, frame.y, frame.width, frame.height, 0, 0, frame.width, frame.height);
+
+    ctx.restore();
+}
