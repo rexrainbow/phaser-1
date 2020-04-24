de5f7134c5adcfaa41a3b7bffa359358f851093b
Richard Davey <rich@photonstorm.com>
2020-04-24 02:16:09 +0100

    Cleared out dead tests

--- dev/index.ts
+++ dev/index.ts
@@ -1,7 +1,3 @@
-// import runTest from './test01';
-// import runTest from './config01';
-// import runTest from './rectangle01';
-
 import runTest from './new world';
 
 runTest();
--- dev/template.ts
+++ dev/template.ts
@@ -1,16 +1,27 @@
-import { Game } from '../src/Game';
-import { StaticScene } from '../src/scenes/StaticScene';
-import { Parent, Size, Scenes, BackgroundColor } from '../src/config';
+import { BackgroundColor, Parent, Scenes, Size } from '../src/config';
 
-class Demo extends StaticScene
+import { AddChild } from '../src/gameobjects/container';
+import { Game } from '../src/Game';
+import { Scene } from '../src/scenes/Scene';
+import { SolidColorTexture } from '../src/textures/types';
+import { Sprite } from '../src/gameobjects/sprite';
+import { World } from '../src/world/World';
+
+class Demo extends Scene
 {
     constructor ()
     {
         super();
+
+        const world = new World(this);
+
+        const block = new Sprite(100, 100, SolidColorTexture('#ff0000', 128, 128));
+
+        AddChild(world, block);
     }
 }
 
-export default function ()
+export default function (): void
 {
     new Game(
         Size(800, 600),
