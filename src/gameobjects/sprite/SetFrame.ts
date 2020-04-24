d7205dab59ebfc5112b4e1db17b8c83fe425bd07
Richard Davey <rich@photonstorm.com>
2020-04-24 01:12:09 +0100

    Pass the texture too and cache the result

--- src/gameobjects/sprite/SetFrame.ts
+++ src/gameobjects/sprite/SetFrame.ts
@@ -1,45 +1,49 @@
 import { Frame } from '../../textures/Frame';
 import { ISprite } from './ISprite';
+import { Texture } from '../../textures';
 
-export function SetFrame (key?: string | number | Frame, ...sprite: ISprite[]): void
+export function SetFrame (texture: Texture, key?: string | number | Frame, ...sprite: ISprite[]): void
 {
+    const frame = texture.get(key);
+
     sprite.forEach(entity =>
     {
-        const frame = entity.texture.get(key);
-
         if (frame === entity.frame)
         {
             return;
         }
-    
+
         entity.frame = frame;
-    
-        entity.setSize(frame.sourceSizeWidth, frame.sourceSizeHeight);
+
+        entity.width = frame.sourceSizeWidth;
+        entity.height = frame.sourceSizeHeight;
+
         entity.setBounds(entity.x, entity.y, entity.width, entity.height);
-    
+
         if (frame.pivot)
         {
-            entity.setOrigin(frame.pivot.x, frame.pivot.y);
+            entity.originX = frame.pivot.x;
+            entity.originY = frame.pivot.y;
         }
-    
+
         const data = entity.vertexData;
-    
+
         //  This rarely changes, so we'll set it here, rather than every game step:
-    
+
         data[2] = frame.u0;
         data[3] = frame.v0;
-    
+
         data[8] = frame.u0;
         data[9] = frame.v1;
-    
+
         data[14] = frame.u1;
         data[15] = frame.v1;
-    
+
         data[20] = frame.u1;
         data[21] = frame.v0;
-    
+
         entity.setDirtyRender();
-    
+
         entity.hasTexture = true;
     });
 }
