dba6e5ca46f5c31af4a6e1f182bdbafa22d92009
Richard Davey <rich@photonstorm.com>
2020-04-29 17:13:06 +0100

    Multiple Entry Points

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@
 
 import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
+import dirTree from 'directory-tree';
 import filesize from 'rollup-plugin-filesize';
 import fs from 'fs-extra';
 import resolve from '@rollup/plugin-node-resolve';
@@ -18,10 +19,10 @@ const extensions = [
 
 const licenseInfo =
 `/**
-* @author       Richard Davey <rich@photonstorm.com>
-* @copyright    2020 Photon Storm Ltd.
-* @license      {@link https://opensource.org/licenses/MIT|MIT License}
-'*/`;
+  * @author       Richard Davey <rich@photonstorm.com>
+  * @copyright    2020 Photon Storm Ltd.
+  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+  */`;
 
 /**
  * Custom Roll-up Plugin that copies the package.json version to the dist package.json
@@ -43,84 +44,164 @@ const DistPackagePlugin = {
 
 };
 
-export default {
+const filterConfig = {
+    extensions: /\.ts/,
+    exclude: /src\\_wip/
+};
 
-    input: './src/index.ts',
+const ESMInputBundle = {};
 
-    output: [
-        {
-            file: './dist/Phaser4.esm.js',
-            format: 'es',
-            sourcemap: true
-        },
-        {
-            file: './dist/Phaser4.js',
-            format: 'umd',
-            name: 'Phaser4',
-            sourcemap: true,
-            plugins: [
-                filesize()
-            ]
-        },
-        {
-            file: './dist/Phaser4.min.js',
-            format: 'umd',
-            name: 'Phaser4',
-            sourcemap: false,
-            plugins: [
-                terser({
-                    output: {
-                        preamble: licenseInfo,
-                        comments: false
-                    }
-                }),
-                DistPackagePlugin
-            ]
-        }
-    ],
+dirTree('src', filterConfig, (item, path) =>
+{
+    /*
+    item structure:
 
-    onwarn: (warning, next) =>
+    { path: 'src\\utils\\base64\\Base64ToArrayBuffer.ts',
+    name: 'Base64ToArrayBuffer.ts',
+    size: 2019,
+    extension: '.ts',
+    type: 'file' }
+    */
+
+    //  First we need to see if this is an interface and bail out
+    //  The quickest test is if the filename starts with a capital I:
+
+    if (item.name.substring(0, 1) === 'I')
     {
-        if (warning.code === 'DEPRECATED_FEATURE')
+        //  Now we need to actually check inside the file *sigh*
+        const fileContents = fs.readFileSync(item.path, 'utf8');
+
+        if (fileContents.includes('export interface'))
         {
+            // console.log('Ignoring interface ' + item.name);
             return;
         }
-        else
-        {
-            next(warning);
-        }
+    }
+
+    const itemPath = path.dirname(item.path);
+
+    //  Take the item path and put it into an array
+    const parts = itemPath.split(path.sep);
+
+    //  Remove the src\ part from the dir array (the first entry)
+    parts.shift();
+
+    //  Turn back, always use / as Rollup requires it
+    const dir = parts.join('/');
+
+    const name = item.name.substring(0, item.name.length - item.extension.length);
+
+    const entryPoint = (dir === '') ? name : dir + '/' + name;
+
+    ESMInputBundle[entryPoint] = itemPath;
+
+    // console.log(entryPoint);
+});
+
+export default [
+    {
+        //  UMD Bundle
+        input: './src/index.ts',
+        output: [
+            {
+                file: './dist/Phaser4.js',
+                format: 'umd',
+                name: 'Phaser4',
+                sourcemap: true,
+                esModule: false,
+                plugins: [
+                    filesize()
+                ]
+            }
+        ],
+        plugins: [
+
+            del({
+                targets: [ './dist', './stats.html' ],
+                runOnce: true
+            }),
+
+            resolve({
+                extensions
+            }),
+
+            typescript({
+                tsconfig: './tsconfig.json'
+            }),
+
+            copy({
+                targets: [
+                    { src: 'LICENSE', dest: 'dist', copyOnce: true },
+                    { src: 'logo.png', dest: 'dist', copyOnce: true },
+                    { src: 'README.dist.md', dest: 'dist', rename: 'README.md', copyOnce: true }
+                ]
+            })
+
+            //  Currently crashing in version 4.0.4, let's hope they fix it
+            // visualizer({
+            //     "title": "Phaser 4 Package Stats",
+            //     "sourcemap": false,
+            //     "template": "treemap" // "circlepacking"
+            // })
+
+        ]
     },
+    {
+        //  UMD Minified Bundle
+        input: './src/index.ts',
 
-    plugins: [
+        output: [
+            {
+                file: './dist/Phaser4.min.js',
+                format: 'umd',
+                name: 'Phaser4',
+                sourcemap: false,
+                esModule: false,
+                plugins: [
+                    terser({
+                        output: {
+                            preamble: licenseInfo,
+                            comments: false
+                        }
+                    }),
+                    DistPackagePlugin
+                ]
+            }
+        ],
 
-        del({
-            targets: [ './dist', './stats.html' ],
-            runOnce: true
-        }),
+        plugins: [
 
-        resolve({
-            extensions
-        }),
+            resolve({
+                extensions
+            }),
 
-        typescript({
-            tsconfig: './tsconfig.json'
-        }),
+            typescript({
+                tsconfig: './tsconfig.json'
+            })
 
-        copy({
-            targets: [
-                { src: 'LICENSE', dest: 'dist', copyOnce: true },
-                { src: 'logo.png', dest: 'dist', copyOnce: true },
-                { src: 'README.dist.md', dest: 'dist', rename: 'README.md', copyOnce: true }
-            ]
-        }),
+        ]
+    },
+    {
+        //  ESM Multiple Entry Point Package
+        input: ESMInputBundle,
 
-        //  Currently crashing in version 4.0.4, let's hope they fix it
-        // visualizer({
-        //     "title": "Phaser 4 Package Stats",
-        //     "sourcemap": false,
-        //     "template": "treemap" // "circlepacking"
-        // })
+        plugins: [
 
-    ]
+            resolve({
+                extensions
+            }),
 
-};
+            typescript({
+                tsconfig: './tsconfig.json'
+            })
+
+        ],
+
+        output: [
+            {
+                dir: 'dist',
+                format: 'esm'
+            }
+        ]
+    }
+];
