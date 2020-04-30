900d90e3fcfbd130dff77aa7501842ccb3321a48
Richard Davey <rich@photonstorm.com>
2020-04-30 13:31:23 +0100

    Expose new functions

--- src/gameobjects/sprite/index.ts
+++ src/gameobjects/sprite/index.ts
@@ -1,19 +1,25 @@
 import { RenderCanvas } from './RenderCanvas';
 import { RenderWebGL } from './RenderWebGL';
+import { SetAlpha } from './SetAlpha';
 import { SetFrame } from './SetFrame';
 import { SetQuadAlpha } from './SetQuadAlpha';
 import { SetQuadTint } from './SetQuadTint';
 import { SetTexture } from './SetTexture';
+import { SetTint } from './SetTint';
 import { Sprite } from './Sprite';
+import { UpdateVertices } from './UpdateVertices';
 import { UploadBuffers } from './UploadBuffers';
 
 export {
     RenderCanvas,
     RenderWebGL,
+    SetAlpha,
     SetFrame,
     SetQuadAlpha,
     SetQuadTint,
     SetTexture,
+    SetTint,
     Sprite,
+    UpdateVertices,
     UploadBuffers
 };
