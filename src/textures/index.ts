6b30bde4d6b6b9e3310c39f2ed5070a63f1a6779
Richard Davey <rich@photonstorm.com>
2020-04-30 15:58:02 +0100

    Expose new functions

--- src/textures/index.ts
+++ src/textures/index.ts
@@ -4,12 +4,20 @@ import * as Types from './types';
 
 import { CreateCanvas } from './CreateCanvas';
 import { Frame } from './Frame';
+import { GLTextureBinding } from './GLTextureBinding';
+import { GetFrames } from './GetFrames';
+import { GetFramesInRange } from './GetFramesInRange';
+import { SetFilter } from './SetFilter';
 import { Texture } from './Texture';
 import { TextureManager } from './TextureManager';
 
 export {
     CreateCanvas,
     Frame,
+    GLTextureBinding,
+    GetFrames,
+    GetFramesInRange,
+    SetFilter,
     Palettes,
     Parsers,
     Types,
