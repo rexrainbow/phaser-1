139854e05ccedfa305021d10cff6a414410890ae
Richard Davey <rich@photonstorm.com>
2020-04-30 13:31:06 +0100

    Extend GO, not Container, set type, add origin and other setters

--- src/gameobjects/sprite/Sprite.ts
+++ src/gameobjects/sprite/Sprite.ts
@@ -1,11 +1,11 @@
-import { Container } from '../container/Container';
 import { Frame } from '../../textures/Frame';
+import { GameObject } from '../GameObject';
 import { IGameObject } from '../IGameObject';
 import { SetFrame } from './SetFrame';
 import { SetTexture } from './SetTexture';
 import { Texture } from '../../textures/Texture';
 
-export class Sprite extends Container
+export class Sprite extends GameObject
 {
     texture: Texture;
     frame: Frame;
@@ -17,13 +17,12 @@ export class Sprite extends Container
     vertexAlpha: Float32Array;
     vertexTint: Uint32Array;
 
-    prevTextureID: number = -1;
-
     private _tint: number = 0xffffff;
+    private _alpha: number = 1;
 
     constructor (x: number, y: number, texture: string | Texture, frame?: string | number)
     {
-        super(x, y);
+        super(x, y, 'Sprite');
 
         this.vertexData = new Float32Array(24).fill(0);
         this.vertexColor = new Uint32Array(4).fill(4294967295);
@@ -31,25 +30,8 @@ export class Sprite extends Container
         this.vertexAlpha = new Float32Array(4).fill(1);
         this.vertexTint = new Uint32Array(4).fill(0xffffff);
 
-        this.type = 'Sprite';
         this.setTexture(texture, frame);
-
-        this.bounds.setArea(x, y, this.width, this.height);
     }
-
-    /*
-    getBounds (includeChildren: boolean = false): Rectangle
-    {
-        if (this.dirtyRender)
-        {
-            this.updateVertices();
-        }
-
-        super.getBounds(includeChildren);
-
-        return this.bounds;
-    }
-    */
 
     setTexture (key: string | Texture, frame?: string | number): this
     {
@@ -70,74 +52,46 @@ export class Sprite extends Container
         return (this.visible && this.willRender && this.hasTexture && this.alpha > 0);
     }
 
-    updateVertices (): void
+    set originX (value: number)
     {
-        const data = this.vertexData;
+        this.transform.setOriginX(value);
 
-        this.dirty.render = false;
-
-        const frame = this.frame;
-        const originX = this.originX;
-        const originY = this.originY;
-
-        let w0: number;
-        let w1: number;
-        let h0: number;
-        let h1: number;
-
-        const { a, b, c, d, tx, ty } = this.transform.world;
-
-        if (frame.trimmed)
+        if (this.frame)
         {
-            w1 = frame.spriteSourceSizeX - (originX * frame.sourceSizeWidth);
-            w0 = w1 + frame.spriteSourceSizeWidth;
-
-            h1 = frame.spriteSourceSizeY - (originY * frame.sourceSizeHeight);
-            h0 = h1 + frame.spriteSourceSizeHeight;
+            this.frame.setExtent(this);
         }
-        else
+    }
+
+    set originY (value: number)
+    {
+        this.transform.setOriginX(value);
+
+        if (this.frame)
         {
-            w1 = -originX * frame.sourceSizeWidth;
-            w0 = w1 + frame.sourceSizeWidth;
-
-            h1 = -originY * frame.sourceSizeHeight;
-            h0 = h1 + frame.sourceSizeHeight;
+            this.frame.setExtent(this);
         }
+    }
 
-        const x0 = (w1 * a) + (h1 * c) + tx;
-        const y0 = (w1 * b) + (h1 * d) + ty;
+    get alpha (): number
+    {
+        return this._alpha;
+    }
 
-        const x1 = (w1 * a) + (h0 * c) + tx;
-        const y1 = (w1 * b) + (h0 * d) + ty;
+    set alpha (value: number)
+    {
+        if (value !== this._alpha)
+        {
+            this._alpha = value;
 
-        const x2 = (w0 * a) + (h0 * c) + tx;
-        const y2 = (w0 * b) + (h0 * d) + ty;
+            const vertexAlpha = this.vertexAlpha;
 
-        const x3 = (w0 * a) + (h1 * c) + tx;
-        const y3 = (w0 * b) + (h1 * d) + ty;
+            vertexAlpha[0] = value;
+            vertexAlpha[1] = value;
+            vertexAlpha[2] = value;
+            vertexAlpha[3] = value;
 
-        //  top left
-        data[0] = x0;
-        data[1] = y0;
-
-        //  bottom left
-        data[6] = x1;
-        data[7] = y1;
-
-        //  bottom right
-        data[12] = x2;
-        data[13] = y2;
-
-        //  top right
-        data[18] = x3;
-        data[19] = y3;
-
-        const boundsX = Math.min(x0, x1, x2, x3);
-        const boundsY = Math.min(y0, y1, y2, y3);
-        const boundsRight = Math.max(x0, x1, x2, x3);
-        const boundsBottom = Math.max(y0, y1, y2, y3);
-
-        this.bounds.setArea(boundsX, boundsY, boundsRight, boundsBottom);
+            this.dirty.setColors();
+        }
     }
 
     get tint (): number
@@ -147,9 +101,19 @@ export class Sprite extends Container
 
     set tint (value: number)
     {
-        this._tint = value;
+        if (value !== this._tint)
+        {
+            this._tint = value;
 
-        // this.setTint(value);
+            const vertexTint = this.vertexTint;
+
+            vertexTint[0] = value;
+            vertexTint[1] = value;
+            vertexTint[2] = value;
+            vertexTint[3] = value;
+
+            this.dirty.setColors();
+        }
     }
 
     destroy (reparentChildren?: IGameObject): void
@@ -165,35 +129,3 @@ export class Sprite extends Container
         this.vertexTint = null;
     }
 }
-
-/*
-    vertexData array structure:
-
-    0 = topLeft.x
-    1 = topLeft.y
-    2 = frame.u0
-    3 = frame.v0
-    4 = textureIndex
-    5 = topLeft.packedColor
-
-    6 = bottomLeft.x
-    7 = bottomLeft.y
-    8 = frame.u0
-    9 = frame.v1
-    10 = textureIndex
-    11 = bottomLeft.packedColor
-
-    12 = bottomRight.x
-    13 = bottomRight.y
-    14 = frame.u1
-    15 = frame.v1
-    16 = textureIndex
-    17 = bottomRight.packedColor
-
-    18 = topRight.x
-    19 = topRight.y
-    20 = frame.u1
-    21 = frame.v0
-    22 = textureIndex
-    23 = topRight.packedColor
-*/
