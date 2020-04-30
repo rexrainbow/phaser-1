37a6b03180ca9865f28b451ed3e8ed2695795fe3
Richard Davey <rich@photonstorm.com>
2020-04-30 13:31:58 +0100

    Generics and can clear texture

--- src/gameobjects/sprite/SetTexture.ts
+++ src/gameobjects/sprite/SetTexture.ts
@@ -4,43 +4,51 @@ import { SetFrame } from './SetFrame';
 import { Texture } from '../../textures/Texture';
 import { TextureManagerInstance } from '../../textures/TextureManagerInstance';
 
-export function SetTexture (key: string | Texture, frame: string | number | Frame, ...sprite: ISprite[]): void
+export function SetTexture <T extends ISprite> (key: string | Texture, frame: string | number | Frame, ...children: T[]): T[]
 {
     if (!key)
     {
-        return;
-    }
-
-    let texture: Texture;
-
-    if (key instanceof Texture)
-    {
-        texture = key;
-    }
-    else
-    {
-        texture = TextureManagerInstance.get().get(key);
-    }
-
-    if (!texture)
-    {
-        console.warn('Invalid Texture key: ' + key);
-
-        return;
-    }
-    else
-    {
-        //  TODO - Move this to the render process
-        if (!texture.glTexture)
+        //  Remove texture from all children
+        children.forEach(child =>
         {
-            texture.createGL();
-        }
-
-        sprite.forEach(entity =>
-        {
-            entity.texture = texture;
-
-            SetFrame(texture, frame, entity);
+            child.texture = null;
+            child.frame = null;
+            child.hasTexture = false;
         });
     }
+    else
+    {
+        let texture: Texture;
+
+        if (key instanceof Texture)
+        {
+            texture = key;
+        }
+        else
+        {
+            texture = TextureManagerInstance.get().get(key);
+        }
+
+        if (!texture)
+        {
+            console.warn('Invalid Texture key: ' + key);
+        }
+        else
+        {
+            //  TODO - Move this to the render process
+            // if (!texture.glTexture)
+            // {
+            //     texture.createGL();
+            // }
+
+            children.forEach(child =>
+            {
+                child.texture = texture;
+            });
+
+            SetFrame(texture, frame, ...children);
+        }
+    }
+
+    return children;
 }
