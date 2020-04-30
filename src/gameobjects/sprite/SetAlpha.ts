0efa3bfa0c749f64010cd5a6625193f7041ddade
Richard Davey <rich@photonstorm.com>
2020-04-30 13:28:48 +0100

    New Alpha and Tint functions

---
+++ src/gameobjects/sprite/SetAlpha.ts
@@ -0,0 +1,19 @@
+import { ISprite } from './ISprite';
+import { PackColors } from '../../renderer/webgl1/PackColors';
+
+export function SetAlpha <T extends ISprite> (alpha: number, ...children: T[]): T[]
+{
+    children.forEach(child =>
+    {
+        const vertexAlpha = child.vertexAlpha;
+
+        vertexAlpha[0] = alpha;
+        vertexAlpha[1] = alpha;
+        vertexAlpha[2] = alpha;
+        vertexAlpha[3] = alpha;
+
+        PackColors(child);
+    });
+
+    return children;
+}
---
+++ src/gameobjects/sprite/SetTint.ts
@@ -0,0 +1,19 @@
+import { ISprite } from './ISprite';
+import { PackColors } from '../../renderer/webgl1/PackColors';
+
+export function SetTint <T extends ISprite> (tint: number, ...children: T[]): T[]
+{
+    children.forEach(child =>
+    {
+        const vertexTint = child.vertexTint;
+
+        vertexTint[0] = tint;
+        vertexTint[1] = tint;
+        vertexTint[2] = tint;
+        vertexTint[3] = tint;
+
+        PackColors(child);
+    });
+
+    return children;
+}
