9a7fee21485991fd9f0ff0104a2fb0be59554945
Richard Davey <rich@photonstorm.com>
2020-05-01 10:14:50 +0100

    Remove Rollup filesize plugin, because it updated, so of course that means its broken

--- fastdev.rollup.config.js
+++ fastdev.rollup.config.js
@@ -1,8 +1,11 @@
 import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
-import filesize from 'rollup-plugin-filesize';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+
+// import filesize from 'rollup-plugin-filesize';
+
+
 
 const extensions = [
     '.js', '.jsx', '.ts', '.tsx'
@@ -19,7 +22,7 @@ export default {
             name: 'Phaser4Example',
             sourcemap: true,
             plugins: [
-                filesize()
+                // filesize()
             ]
         }
     ],
