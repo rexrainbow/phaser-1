c56f1bfdbf2fd9e893ebfb30d85044ab10c92e08
Richard Davey <rich@photonstorm.com>
2020-05-01 13:17:42 +0100

    Update index.ts

--- src/gameobjects/components/transform/index.ts
+++ src/gameobjects/components/transform/index.ts
@@ -1,8 +1,10 @@
+import { GetVertices } from './GetVertices';
 import { TransformComponent } from './TransformComponent';
 import { UpdateLocalTransform } from './UpdateLocalTransform';
 import { UpdateWorldTransform } from './UpdateWorldTransform';
 
 export {
+    GetVertices,
     TransformComponent,
     UpdateLocalTransform,
     UpdateWorldTransform
