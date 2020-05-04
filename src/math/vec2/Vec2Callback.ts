a4bbb17950afc17a328329b97bee8b7bd1b85759
Richard Davey <rich@photonstorm.com>
2020-05-04 10:51:50 +0100

    Testing Vec2Callback

---
+++ src/math/vec2/Vec2Callback.ts
@@ -0,0 +1,62 @@
+import { NOOP } from '../../utils/NOOP';
+
+export class Vec2Callback
+{
+    private _x: number;
+    private _y: number;
+    callback: (vec2: Vec2Callback) => void;
+    compareValue: boolean = false;
+
+    constructor (callback: (vec2: Vec2Callback) => void, x: number = 0, y: number = 0, compareValue: boolean = false)
+    {
+        this.callback = callback;
+        this.compareValue = compareValue;
+
+        this.set(x, y);
+    }
+
+    set (x: number = 0, y: number = 0): this
+    {
+        this._x = x;
+        this._y = y;
+
+        this.callback(this);
+
+        return this;
+    }
+
+    destroy (): void
+    {
+        this.callback = NOOP;
+    }
+
+    set x (value: number)
+    {
+        if (this.compareValue && value !== this._x)
+        {
+            this._x = value;
+
+            this.callback(this);
+        }
+    }
+
+    get x (): number
+    {
+        return this._x;
+    }
+
+    set y (value: number)
+    {
+        if (this.compareValue && value !== this._y)
+        {
+            this._y = value;
+
+            this.callback(this);
+        }
+    }
+
+    get y (): number
+    {
+        return this._y;
+    }
+}
--- src/math/vec2/index.ts
+++ src/math/vec2/index.ts
@@ -1,5 +1,7 @@
 import { Vec2 } from './Vec2';
+import { Vec2Callback } from './Vec2Callback';
 
 export {
-    Vec2
+    Vec2,
+    Vec2Callback
 };
