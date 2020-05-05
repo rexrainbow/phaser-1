b3dbce0cc3cbc1e66856652f4dc29db4a6ae0a0c
Richard Davey <rich@photonstorm.com>
2020-05-05 12:40:21 +0100

    Create SpriteBatchAddConfig.ts

---
+++ src/gameobjects/spritebatch/SpriteBatchAddConfig.ts
@@ -0,0 +1,16 @@
+import { Frame } from '../../textures/Frame';
+
+export type SpriteBatchAddConfig = {
+    frame?: string | number;
+    x?: number;
+    y?: number;
+    rotation?: number;
+    scaleX?: number;
+    scaleY?: number;
+    skewX?: number;
+    skewY?: number;
+    originX?: number;
+    originY?: number;
+    alpha?: number;
+    tint?: number;
+};
