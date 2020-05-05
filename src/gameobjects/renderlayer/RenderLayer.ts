e5551b5e5757943837a63969b08ff0b9a4424bce
Richard Davey <rich@photonstorm.com>
2020-05-05 15:58:19 +0100

    Update RenderLayer.ts

--- src/gameobjects/renderlayer/RenderLayer.ts
+++ src/gameobjects/renderlayer/RenderLayer.ts
@@ -1,26 +1,11 @@
 import { Layer } from '../layer/Layer';
-import { Texture } from '../../textures/Texture';
 
 export class RenderLayer extends Layer
 {
-
-
     constructor ()
     {
         super();
 
         this.type = 'RenderLayer';
     }
-
-    draw (x: number, y: number, texture: string | Texture, frame?: string | number): this
-    {
-
-
-        return this;
-    }
-
-    //  drawWithTransformConfig
-
-
-
 }
