09a51d0226876a6b243bd48fd3162b8cb7efbb91
Richard Davey <rich@photonstorm.com>
2020-05-05 15:57:33 +0100

    Can remove an EventInstance directly

--- src/events/Off.ts
+++ src/events/Off.ts
@@ -1,17 +1,23 @@
+import { EventInstance } from './EventInstance';
 import { IEventEmitter } from './IEventEmitter';
+import { IEventInstance } from './IEventInstance';
 
-export function Off (emitter: IEventEmitter, event: string, callback?: Function, context?: unknown, once?: boolean): IEventEmitter
+export function Off (emitter: IEventEmitter, event: string, callback?: Function | IEventInstance, context?: unknown, once?: boolean): IEventEmitter
 {
     const events = emitter.events;
+    const listeners = events.get(event);
 
     if (!callback)
     {
         //  Remove all events matching the given key
         events.delete(event);
     }
+    else if (callback instanceof EventInstance)
+    {
+        listeners.delete(callback as IEventInstance);
+    }
     else
     {
-        const listeners = events.get(event);
         const hasContext: boolean = !context;
         const hasOnce: boolean = (once !== undefined);
 
@@ -19,18 +25,18 @@ export function Off (emitter: IEventEmitter, event: string, callback?: Function,
         {
             if (
                 (listener.callback === callback) &&
-                (hasContext && listener.context === console) &&
+                (hasContext && listener.context === context) &&
                 (hasOnce && listener.once === once)
             )
             {
                 listeners.delete(listener);
             }
         }
+    }
 
-        if (listeners.size === 0)
-        {
-            events.delete(event);
-        }
+    if (listeners.size === 0)
+    {
+        events.delete(event);
     }
 
     return emitter;
