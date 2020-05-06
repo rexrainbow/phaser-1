b1c0aa0e97fb2c47bc9044b98a0208e78c86bab6
Richard Davey <rich@photonstorm.com>
2020-05-06 18:07:40 +0100

    Create EffectLayer.ts

---
+++ src/gameobjects/effectlayer/EffectLayer.ts
@@ -0,0 +1,13 @@
+import { Layer } from '../layer/Layer';
+
+export class EffectLayer extends Layer
+{
+    constructor ()
+    {
+        super();
+
+        this.type = 'EffectLayer';
+
+
+    }
+}
