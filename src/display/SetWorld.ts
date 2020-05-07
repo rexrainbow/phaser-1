c7fdc2b0465aad3c196856c054279eceed279d54
Richard Davey <rich@photonstorm.com>
2020-05-07 18:03:25 +0100

    Emit add and remove from world events

--- src/display/SetWorld.ts
+++ src/display/SetWorld.ts
@@ -1,11 +1,21 @@
+import { Emit } from '../events/Emit';
 import { IBaseWorld } from '../world/IBaseWorld';
 import { IGameObject } from '../gameobjects/IGameObject';
 
 export function SetWorld <T extends IGameObject> (world: IBaseWorld, ...children: T[]): T[]
 {
+    const worldScene = world.scene;
+
     children.forEach(child =>
     {
+        if (child.world)
+        {
+            Emit(child.world.scene, 'removedfromworld', child, child.world);
+        }
+
         child.world = world;
+
+        Emit(worldScene, 'addedtoworld', child, world);
     });
 
     return children;
