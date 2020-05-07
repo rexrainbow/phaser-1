bbac018ba40493b98f6d121b3f7910ae6b2e4ce1
Richard Davey <rich@photonstorm.com>
2020-05-07 18:03:37 +0100

    Added pending and post render support

--- src/gameobjects/components/dirty/DirtyComponent.ts
+++ src/gameobjects/components/dirty/DirtyComponent.ts
@@ -1,11 +1,14 @@
 import { GameInstance } from '../../../GameInstance';
 import { IDirtyComponent } from './IDirtyComponent';
 import { IGameObject } from '../../IGameObject';
+import { IWorldRenderData } from '../../../world/IWorldRenderData';
 
 export class DirtyComponent implements IDirtyComponent
 {
     entity: IGameObject;
     render: boolean = true;
+    pendingRender: boolean = false;
+    postRender: boolean = false;
     update: boolean = true;
     colors: boolean = true;
     frame: number = 0;
@@ -13,6 +16,29 @@ export class DirtyComponent implements IDirtyComponent
     constructor (entity: IGameObject)
     {
         this.entity = entity;
+    }
+
+    setPendingRender (renderData: IWorldRenderData): void
+    {
+        if (this.pendingRender)
+        {
+            return;
+        }
+
+        this.pendingRender = true;
+
+        renderData.numRendered++;
+        renderData.numRenderable++;
+
+        if (this.frame >= renderData.gameFrame)
+        {
+            renderData.dirtyFrame++;
+        }
+    }
+
+    setPostRender (): void
+    {
+        this.postRender = true;
     }
 
     setRender (): void
--- src/gameobjects/components/dirty/IDirtyComponent.ts
+++ src/gameobjects/components/dirty/IDirtyComponent.ts
@@ -1,13 +1,18 @@
 import { IGameObject } from '../../IGameObject';
+import { IWorldRenderData } from '../../../world/IWorldRenderData';
 
 export interface IDirtyComponent
 {
     entity: IGameObject;
     render: boolean;
+    pendingRender: boolean;
+    postRender: boolean;
     update: boolean;
     colors: boolean;
     frame: number;
     setRender (): void;
+    setPendingRender (renderData: IWorldRenderData): void;
+    setPostRender (): void;
     setUpdate (): void;
     setColors (): void;
     destroy (): void;
