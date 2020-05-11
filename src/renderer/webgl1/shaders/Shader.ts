422ecf8cfc8eb047adaef325358ccdef72f65209
Richard Davey <rich@photonstorm.com>
2020-05-11 18:03:20 +0100

    Update Shader.ts

--- src/renderer/webgl1/shaders/Shader.ts
+++ src/renderer/webgl1/shaders/Shader.ts
@@ -1,34 +1,17 @@
+import { IShader } from './IShader';
 import { IShaderConfig } from './IShaderConfig';
 import { SingleTextureQuadShader } from './SingleTextureQuadShader';
 import { WebGLRenderer } from '../WebGLRenderer';
 
-const fragmentShader = `
-precision highp float;
-
-varying vec2 vTextureCoord;
-varying float vTextureId;
-varying vec4 vTintColor;
-
-uniform sampler2D uTexture;
-
-void main (void)
-{
-    vec4 color = texture2D(uTexture, vTextureCoord);
-
-    gl_FragColor = color * vec4(vTintColor.bgr * vTintColor.a, vTintColor.a);
-}`;
-
-export class Shader extends SingleTextureQuadShader
+export class Shader extends SingleTextureQuadShader implements IShader
 {
     renderer: WebGLRenderer;
     gl: WebGLRenderingContext;
 
     program: WebGLProgram;
 
-    constructor (renderer: WebGLRenderer, config: IShaderConfig = {})
+    constructor (config: IShaderConfig = {})
     {
-        config.fragmentShader = fragmentShader;
-
-        super(renderer, config);
+        super(config);
     }
 }
