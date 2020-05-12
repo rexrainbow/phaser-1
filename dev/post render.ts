99f2304ecdab6aaee582e1a510596f697482524b
Richard Davey <rich@photonstorm.com>
2020-05-12 16:20:07 +0100

    New shader test

--- dev/post render.ts
+++ dev/post render.ts
@@ -9,6 +9,31 @@ import { Scene } from '../src/scenes/Scene';
 import { Shader } from '../src/renderer/webgl1/shaders/Shader';
 import { StaticWorld } from '../src/world/StaticWorld';
 
+const lazerBeamFragmentShader = `
+precision highp float;
+
+varying vec2 vTextureCoord;
+varying float vTextureId;
+varying vec4 vTintColor;
+
+uniform sampler2D uTexture;
+uniform float uTime;
+uniform vec2 uResolution;
+
+void main (void)
+{
+    vec4 color = texture2D(uTexture, vTextureCoord);
+
+    vec2 p = (gl_FragCoord.yx / uResolution.yx) - 0.5;
+
+    float sx = 0.3 * (p.x + 0.8) * sin(900.0 * p.x - 1.0 * pow(uTime, 0.55) * 5.0);
+    float dy = 4.0 / (500.0 * abs(p.y - sx));
+
+    dy += 1.0 / (25.0 * length(p - vec2(p.x, 0.0)));
+
+    gl_FragColor = color * vec4((p.x + 0.1) * dy, 0.3 * dy, dy, 1.1);
+}`;
+
 const redFragmentShader = `
 precision highp float;
 
@@ -102,27 +127,30 @@ class Demo extends Scene
         const red = new Shader({ fragmentShader: redFragmentShader, batchSize: 1 });
         const blurX = new Shader({ fragmentShader: blurXFragmentShader, batchSize: 1 });
         const blurY = new Shader({ fragmentShader: blurYFragmentShader, batchSize: 1 });
+        const lazer = new Shader({ fragmentShader: lazerBeamFragmentShader, batchSize: 1 });
 
         const world = new StaticWorld(this);
 
         const layer = new EffectLayer();
 
-        layer.shaders.push(red, blurX, blurY);
+        // layer.shaders.push(red, blurX, blurY);
+        layer.shaders.push(lazer);
 
-        const ayu1 = new Sprite(400, 100, 'ayu');
+        const ayu1 = new Sprite(400, 300, 'ayu');
         const ayu2 = new Sprite(480, 500, 'ayu');
 
         const bubble = new Sprite(400, 300, 'bubble');
         const logo = new Sprite(400, 300, 'logo');
 
+        AddChildren(layer, ayu1);
         // AddChildren(layer, logo);
 
-        AddChildren(layer, logo, bubble);
+        // AddChildren(layer, logo, bubble);
         // AddChildren(world, ayu1, ayu2, logo, bubble);
 
-        AddChild(world, ayu1);
+        // AddChild(world, ayu1);
         AddChild(world, layer);
-        AddChild(world, ayu2);
+        // AddChild(world, ayu2);
     }
 }
 
