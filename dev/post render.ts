72e07d0a0f72a41004897749f6c1d22d1019e314
Richard Davey <rich@photonstorm.com>
2020-05-12 01:51:36 +0100

    Update post render.ts

--- dev/post render.ts
+++ dev/post render.ts
@@ -1,5 +1,5 @@
 import { AddChild, AddChildren } from '../src/display/';
-import { BackgroundColor, CanvasRenderer, Parent, Scenes, Size, WebGLRenderer } from '../src/config';
+import { BackgroundColor, Parent, Scenes, Size, WebGLRenderer } from '../src/config';
 import { EffectLayer, Sprite } from '../src/gameobjects/';
 
 import { Game } from '../src/Game';
@@ -9,7 +9,7 @@ import { Scene } from '../src/scenes/Scene';
 import { Shader } from '../src/renderer/webgl1/shaders/Shader';
 import { StaticWorld } from '../src/world/StaticWorld';
 
-const fragmentShader = `
+const redFragmentShader = `
 precision highp float;
 
 varying vec2 vTextureCoord;
@@ -22,8 +22,61 @@ void main (void)
 {
     vec4 color = texture2D(uTexture, vTextureCoord);
 
-    // gl_FragColor = color * vec4(vTintColor.bgr * vTintColor.a, vTintColor.a);
     gl_FragColor = color * vec4(1.0, 0.0, 0.0, 1.0);
+}`;
+
+const blurXFragmentShader = `
+precision mediump float;
+
+varying vec2 vTextureCoord;
+varying float vTextureId;
+varying vec4 vTintColor;
+
+uniform sampler2D uTexture;
+
+void main (void)
+{
+    vec4 sum = vec4(0.0);
+    float blur = 0.001953125;
+
+    sum += texture2D(uTexture, vec2(vTextureCoord.x - 4.0 * blur, vTextureCoord.y)) * 0.05;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x - 3.0 * blur, vTextureCoord.y)) * 0.09;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x - 2.0 * blur, vTextureCoord.y)) * 0.12;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x - blur, vTextureCoord.y)) * 0.15;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y)) * 0.16;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x + blur, vTextureCoord.y)) * 0.15;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x + 2.0 * blur, vTextureCoord.y)) * 0.12;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x + 3.0 * blur, vTextureCoord.y)) * 0.09;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x + 4.0 * blur, vTextureCoord.y)) * 0.05;
+
+    gl_FragColor = sum;
+}`;
+
+const blurYFragmentShader = `
+precision mediump float;
+
+varying vec2 vTextureCoord;
+varying float vTextureId;
+varying vec4 vTintColor;
+
+uniform sampler2D uTexture;
+
+void main (void)
+{
+    vec4 sum = vec4(0.0);
+    float blur = 0.001953125;
+
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y - 4.0 * blur)) * 0.05;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y - 3.0 * blur)) * 0.09;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y - 2.0 * blur)) * 0.12;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y - blur)) * 0.15;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y)) * 0.16;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y + blur)) * 0.15;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y + 2.0 * blur)) * 0.12;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y + 3.0 * blur)) * 0.09;
+    sum += texture2D(uTexture, vec2(vTextureCoord.x, vTextureCoord.y + 4.0 * blur)) * 0.05;
+
+    gl_FragColor = sum;
 }`;
 
 class Demo extends Scene
@@ -46,14 +99,15 @@ class Demo extends Scene
 
     create ()
     {
-        const shader = new Shader({ fragmentShader });
-        // const shader = new Shader();
+        const red = new Shader({ fragmentShader: redFragmentShader, batchSize: 1 });
+        const blurX = new Shader({ fragmentShader: blurXFragmentShader, batchSize: 1 });
+        const blurY = new Shader({ fragmentShader: blurYFragmentShader, batchSize: 1 });
 
         const world = new StaticWorld(this);
 
         const layer = new EffectLayer();
 
-        layer.shader = shader;
+        layer.shaders.push(red, blurX, blurY);
 
         const ayu1 = new Sprite(400, 100, 'ayu');
         const ayu2 = new Sprite(480, 500, 'ayu');
