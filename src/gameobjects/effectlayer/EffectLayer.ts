580db54a28701fb89236980389bb1ae274d5f2b0
Richard Davey <rich@photonstorm.com>
2020-05-15 15:23:31 +0100

    Added DrawQuadTexture for WebGL

--- src/gameobjects/effectlayer/EffectLayer.ts
+++ src/gameobjects/effectlayer/EffectLayer.ts
@@ -1,6 +1,7 @@
 import { GetHeight, GetResolution, GetWidth } from '../../config';
 
 import { CreateFramebuffer } from '../../renderer/webgl1/fbo/CreateFramebuffer';
+import { DrawTexturedQuad } from '../../renderer/webgl1/draw/DrawTexturedQuad';
 import { GLTextureBinding } from '../../renderer/webgl1/textures/GLTextureBinding';
 import { IShader } from '../../renderer/webgl1/shaders/IShader';
 import { IWebGLRenderer } from '../../renderer/webgl1/IWebGLRenderer';
@@ -47,8 +48,6 @@ export class EffectLayer extends Layer
 
         if (this.numChildren > 0)
         {
-            // console.log('EL fbo.add', renderer.fbo);
-
             renderer.fbo.add(this.framebuffer, true);
         }
     }
@@ -57,16 +56,9 @@ export class EffectLayer extends Layer
     {
         super.postRender(renderer);
 
-        renderer.flush();
-
         const shaders = this.shaders;
         const texture = this.texture;
         const { u0, v0, u1, v1 } = texture.firstFrame;
-        const packedColor = 4294967295;
-
-        renderer.textures.bindFBOTexture(texture);
-
-        const textureIndex = texture.binding.index;
 
         if (shaders.length === 0)
         {
@@ -77,44 +69,11 @@ export class EffectLayer extends Layer
             shaders.forEach(shader =>
             {
                 //  Render all the children now
-                renderer.setShader(shader, textureIndex);
+                renderer.setShader(shader, 0);
 
-                const F32 = shader.buffer.vertexViewF32;
-                const U32 = shader.buffer.vertexViewU32;
+                renderer.textures.bindFBOTexture(texture);
 
-                //  top left
-                F32[0] = 0;
-                F32[1] = 0;
-                F32[2] = u0;
-                F32[3] = v1;
-                F32[4] = textureIndex;
-                U32[5] = packedColor;
-
-                //  bottom left
-                F32[6] = 0;
-                F32[7] = texture.height;
-                F32[8] = u0;
-                F32[9] = v0;
-                F32[10] = textureIndex;
-                U32[11] = packedColor;
-
-                //  bottom right
-                F32[12] = texture.width;
-                F32[13] = texture.height;
-                F32[14] = u1;
-                F32[15] = v0;
-                F32[16] = textureIndex;
-                U32[17] = packedColor;
-
-                //  top right
-                F32[18] = texture.width;
-                F32[19] = 0;
-                F32[20] = u1;
-                F32[21] = v1;
-                F32[22] = textureIndex;
-                U32[23] = packedColor;
-
-                shader.count = 1;
+                DrawTexturedQuad(renderer, texture.width, texture.height, u0, v0, u1, v1);
 
                 renderer.fbo.pop();
 
