ea1b69355058100ec8871bc57e2e8ad0960cdcda
Richard Davey <rich@photonstorm.com>
2020-05-18 17:46:23 +0100

    Reworking to handle root node setting

--- src/display/SetParent.ts
+++ src/display/SetParent.ts
@@ -1,9 +1,12 @@
+import { DepthFirstSearch } from './DepthFirstSearch';
 import { IGameObject } from '../gameobjects/IGameObject';
 import { RemoveChild } from './RemoveChild';
 import { SetWorld } from './SetWorld';
 
 export function SetParent (parent: IGameObject, ...children: IGameObject[]): IGameObject[]
 {
+    const parentWorld = parent.world;
+
     children.forEach(child =>
     {
         if (child.parent)
@@ -14,11 +17,21 @@ export function SetParent (parent: IGameObject, ...children: IGameObject[]): IGa
         child.parent = parent;
     });
 
-    const parentWorld = parent.world;
+    //  Full list of all children, including sub-children
+    const childrenList = DepthFirstSearch(parent);
 
     if (parentWorld)
     {
-        SetWorld(parentWorld, ...children);
+        SetWorld(parentWorld, ...childrenList);
+    }
+
+    //  If the parent is not the World, then set it as the root node on all children, all the way down
+    if (parent !== parentWorld)
+    {
+        childrenList.forEach(child =>
+        {
+            child.root = parent.root;
+        });
     }
 
     return children;
