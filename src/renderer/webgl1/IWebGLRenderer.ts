e3cbc8312181f43140fe7f608a1a5bc2dd444042
Richard Davey <rich@photonstorm.com>
2020-05-18 13:19:49 +0100

    Update IWebGLRenderer.ts

--- src/renderer/webgl1/IWebGLRenderer.ts
+++ src/renderer/webgl1/IWebGLRenderer.ts
@@ -1,9 +1,7 @@
 import { FBOSystem } from './fbo/FBOSystem';
 import { IBaseCamera } from '../../camera/IBaseCamera';
 import { IRenderer } from '../IRenderer';
-import { IShader } from './shaders/IShader';
 import { ShaderSystem } from './shaders/ShaderSystem';
-import { SingleTextureQuadShader } from './shaders/SingleTextureQuadShader';
 import { TextureSystem } from './textures/TextureSystem';
 
 export interface IWebGLRenderer extends IRenderer
@@ -14,10 +12,6 @@ export interface IWebGLRenderer extends IRenderer
     textures: TextureSystem;
     shaders: ShaderSystem;
 
-    // singleQuadShader: SingleTextureQuadShader;
-    // currentShader: IShader;
-    // shaders: IShader[];
-
     projectionMatrix: Float32Array;
     flushTotal: number;
     contextLost: boolean;
@@ -26,8 +20,5 @@ export interface IWebGLRenderer extends IRenderer
     onContextLost (event: Event): void;
     onContextRestored (): void;
     reset (framebuffer?: WebGLFramebuffer, width?: number, height?: number): void;
-    // setShader (newShader: IShader, textureID?: number): IShader;
-    // popShader (): void;
-    // resetShader (): void;
     flush (): void;
 }
