e0e14073d81f032da2dcce805d792f26f7f0221c
Richard Davey <rich@photonstorm.com>
2020-05-19 16:48:28 +0100

    Lots of changes for the new tree search system, allowing us to cull the render list easily

--- src/world/BaseWorld.ts
+++ src/world/BaseWorld.ts
@@ -13,6 +13,7 @@ import { IWorldRenderData } from './IWorldRenderData';
 import { MergeRenderData } from './MergeRenderData';
 import { RemoveChildren } from '../display';
 import { ResetWorldRenderData } from './ResetWorldRenderData';
+import { SearchEntry } from '../display/DepthFirstSearchRecursiveNested';
 
 export class BaseWorld extends GameObject implements IBaseWorld
 {
@@ -26,7 +27,9 @@ export class BaseWorld extends GameObject implements IBaseWorld
     private _updateListener: IEventInstance;
     private _renderListener: IEventInstance;
     private _shutdownListener: IEventInstance;
-    protected _stack: IGameObject[];
+
+    protected _stack: SearchEntry[];
+    protected _cachedLayers: SearchEntry[];
 
     constructor (scene: IScene)
     {
@@ -38,66 +41,153 @@ export class BaseWorld extends GameObject implements IBaseWorld
 
         this.clock = new Clock(this);
 
-        this._stack = [];
-
         this._updateListener = On(scene, 'update', (delta: number, time: number) => this.update(delta, time));
         this._renderListener = On(scene, 'render', (renderData: ISceneRenderData) => this.sceneRender(renderData));
         this._shutdownListener = On(scene, 'shutdown', () => this.shutdown());
         Once(scene, 'destroy', () => this.destroy());
     }
 
-    addNode (node: IGameObject, renderData: IWorldRenderData): boolean
+    depthFirstSearch (parent: IGameObject, output: SearchEntry[] = []): SearchEntry[]
     {
-        if (node.isRenderable())
+        for (let i = 0; i < parent.numChildren; i++)
         {
-            renderData.renderList.push(node);
+            const node = parent.children[i];
 
-            return (!node.isDirty(DIRTY_CONST.PENDING_RENDER));
+            if (node.isRenderable())
+            {
+                const children: SearchEntry[] = [];
+
+                const entry = { node, children };
+
+                output.push(entry);
+
+                if (node.willRenderChildren && node.numChildren > 0)
+                {
+                    if (node.willCacheChildren)
+                    {
+                        this._cachedLayers.push(entry);
+                    }
+
+                    this.depthFirstSearch(node, children);
+                }
+            }
         }
+
+        return output;
+    }
+
+    calculateTotal (entry: SearchEntry, renderData: IWorldRenderData): void
+    {
+        renderData.numRendered++;
+        renderData.numRenderable++;
+
+        if (entry.node.dirtyFrame >= renderData.gameFrame)
+        {
+            renderData.dirtyFrame++;
+        }
+
+        entry.children.forEach(child =>
+        {
+            if (child.children.length > 0)
+            {
+                this.calculateTotal(child, renderData);
+            }
+        });
+    }
+
+    updateCachedLayers (): void
+    {
+        const dirtyCamera = this.camera.dirtyRender;
+        const layers = this._cachedLayers;
+
+        layers.forEach(entry =>
+        {
+            if (dirtyCamera || this.hasDirtyChildren(entry))
+            {
+                //  Camera is dirty, or layer has at least one dirty child
+                entry.node.setDirty(DIRTY_CONST.CHILD_CACHE);
+            }
+            else
+            {
+                //  Camera is clean and no dirty children, so we can re-use layer cache
+                //  So let's remove the children for this layer
+                entry.children.length = 0;
+            }
+        });
+    }
+
+    hasDirtyChildren (parent: SearchEntry): boolean
+    {
+        if (parent.node.isDirty(DIRTY_CONST.CHILD_CACHE))
+        {
+            return true;
+        }
+
+        const stack = [ parent ];
+
+        while (stack.length > 0)
+        {
+            const entry = stack.pop();
+
+            if (entry.node.isDirty(DIRTY_CONST.TRANSFORM))
+            {
+                return true;
+            }
+
+            const numChildren = entry.children.length;
+
+            if (numChildren > 0)
+            {
+                for (let i = 0; i < numChildren; i++)
+                {
+                    stack.push(entry.children[i]);
+                }
+            }
+        }
+
+        stack.length = 0;
 
         return false;
     }
 
-    //  Depth First Search with stack instead of recursion
-
     buildRenderList (renderData: IWorldRenderData): void
     {
-        const stack = this._stack;
+        //  entries is now populated with the n-tree search results, only containing nodes that will actually render
+        const entries = this.depthFirstSearch(this, this._stack);
 
-        while (stack.length > 0)
+        //  We can now sweep through the entries and purge non-dirty children of cached layers,
+        //  before finally building the render list. We can only do this if the camera is clean.
+
+        if (this._cachedLayers.length > 0)
         {
-            const node = stack.shift();
+            this.updateCachedLayers();
+        }
 
-            if (this.addNode(node, renderData))
+        //  numRenderable probably needs to move to the search function
+        entries.forEach(entry =>
+        {
+            if (entry.children.length)
+            {
+                this.calculateTotal(entry, renderData);
+            }
+            else
             {
                 renderData.numRendered++;
                 renderData.numRenderable++;
 
-                if (node.dirtyFrame >= renderData.gameFrame)
+                if (entry.node.dirtyFrame >= renderData.gameFrame)
                 {
                     renderData.dirtyFrame++;
                 }
-
-                node.setDirty(DIRTY_CONST.PENDING_RENDER);
             }
+        });
 
-            const numChildren = node.numChildren;
+        renderData.renderList = entries;
 
-            if (!node.isDirty(DIRTY_CONST.POST_RENDER) && node.visible && node.willRenderChildren && numChildren > 0)
-            {
-                //  Inject postRender hook
-                node.setDirty(DIRTY_CONST.POST_RENDER);
+        // console.log(entries);
 
-                stack.unshift(node);
-
-                for (let i = numChildren - 1; i >= 0; i--)
-                {
-                    const child = node.children[i];
-
-                    stack.unshift(child);
-                }
-            }
-        }
+        // eslint-disable-next-line no-debugger
+        // debugger;
     }
 
     update (delta: number, time: number): void
@@ -123,7 +213,8 @@ export class BaseWorld extends GameObject implements IBaseWorld
             return;
         }
 
-        this._stack = [ this ];
+        this._stack = [];
+        this._cachedLayers = [];
 
         this.buildRenderList(renderData);
 
--- src/world/IBaseWorld.ts
+++ src/world/IBaseWorld.ts
@@ -4,6 +4,7 @@ import { IGameObject } from '../gameobjects/IGameObject';
 import { IScene } from '../scenes/IScene';
 import { ISceneRenderData } from '../scenes/ISceneRenderData';
 import { IWorldRenderData } from './IWorldRenderData';
+import { SearchEntry } from '../display/DepthFirstSearchRecursiveNested';
 
 export interface IBaseWorld extends IGameObject
 {
@@ -12,7 +13,10 @@ export interface IBaseWorld extends IGameObject
     camera: IBaseCamera;
     renderData: IWorldRenderData;
     forceRefresh: boolean;
-    addNode (node: IGameObject, renderData: IWorldRenderData): boolean;
+    depthFirstSearch (parent: IGameObject, output?: SearchEntry[]): SearchEntry[];
+    calculateTotal (entry: SearchEntry, renderData: IWorldRenderData): void;
+    updateCachedLayers (): void;
+    hasDirtyChildren (parent: SearchEntry): boolean;
     buildRenderList (renderData: IWorldRenderData): void;
     sceneRender (sceneRenderData: ISceneRenderData): void;
     shutdown (): void;
