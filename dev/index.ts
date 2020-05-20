f75425291bcd04add0efd14a188c93b872bba891
Richard Davey <rich@photonstorm.com>
2020-05-20 13:00:08 +0100

    Testing tween class

--- dev/index.ts
+++ dev/index.ts
@@ -26,7 +26,9 @@
 
 // import runTest from './over 16 textures';
 
-import runTest from './effect layer 1';
+// import runTest from './effect layer 1';
+
+import runTest from './tween 1';
 
 // import runTest from './render layer 1';
 
---
+++ dev/tween 1.ts
@@ -0,0 +1,652 @@
+import { AddChild, AddChildren } from '../src/display/';
+import { BackgroundColor, CanvasRenderer, Parent, Scenes, Size, WebGLRenderer } from '../src/config';
+
+import { Game } from '../src/Game';
+import { IBaseWorld } from '../src/world/IBaseWorld';
+import { IEventInstance } from '../src/events/IEventInstance';
+import { ImageFile } from '../src/loader/files/ImageFile';
+import { Loader } from '../src/loader/Loader';
+import { On } from '../src/events';
+import { Scene } from '../src/scenes/Scene';
+import { Sprite } from '../src/gameobjects/';
+import { StaticWorld } from '../src/world/StaticWorld';
+
+class Quadratic
+{
+    static easeIn (k: number): number
+    {
+        return k * k;
+    }
+
+    static easeOut (k: number): number
+    {
+        return -k * (k - 2);
+    }
+
+    static easeInOut (k: number): number
+    {
+        if ((k *= 2) < 1) return 0.5 * k * k;
+
+        return -0.5 * (--k * (k - 2) - 1);
+    }
+}
+
+type Duration = {
+    slow: number;
+    normal: number;
+    fast: number;
+}
+
+class TweenProperty
+{
+	name: string;
+	start: number;
+	end: number;
+	delta: number;
+	next: TweenProperty;
+
+	constructor (name: string, end: number, next: TweenProperty)
+	{
+		this.name = name;
+		this.end = end;
+		this.next = next;
+	}
+
+	init (target: object, reversed: boolean): void
+	{
+		if (reversed)
+		{
+			this.start = this.end;
+			this.end = target[this.name];
+			target[this.name] = this.start;
+		}
+        else
+        {
+            this.start = target[this.name];
+        }
+
+		this.delta = this.end - this.start;
+	}
+
+	dispose (): void
+	{
+        if (this.next)
+        {
+            this.next.dispose();
+        }
+
+        this.next = null;
+	}
+}
+
+class CompleteData
+{
+	private callback: Function;
+	private args: [];
+	private chain: Tween[];
+	private diff: number;
+
+	constructor (callback: Function, args: [], chain: Tween[], diff: number)
+	{
+		this.callback = callback;
+		this.args = args;
+		this.chain = chain;
+		this.diff = diff;
+	}
+
+	execute (): void
+	{
+		if (this.callback)
+		{
+			this.callback.apply(null, this.args);
+			this.callback = null;
+		}
+
+        this.args = null;
+
+        if (this.chain)
+		{
+			const len: number = this.chain.length;
+
+            for (let i: number = 0; i < len; i++)
+            {
+                new Tween(this.chain[i]).start(false, this.diff);
+            }
+
+            this.chain = null;
+		}
+	}
+}
+
+class Tween
+{
+    static defaultEasing: Function = Quadratic.easeOut;
+    static defaultDuration: Duration = { slow: 1, normal: 0.4, fast: 0.2 };
+
+    private static running: Map<any, Tween> = new Map();
+    private static head: Tween;
+    private static tweenCount: number = 0;
+    private static world: IBaseWorld;
+    private static _updateListener: IEventInstance;
+
+    private prev: Tween;
+    private next: Tween;
+    private rnext: Tween;
+    private isDead: boolean;
+
+    private target: object;
+    private reversed: boolean;
+    private overwrite: boolean;
+    private autoStart: boolean;
+    private _configured: boolean;
+    private _started: boolean;
+    private _inited: boolean;
+    private duration: any;
+    private _duration: number;
+    private _ease: Function;
+    private startTime: number;
+    private endTime: number;
+    private properties: TweenProperty;
+    // private specials: EazeSpecial;
+    // private autoVisible: Boolean;
+    // private slowTween: Boolean;
+    private _chain: Tween[];
+
+    private _onStart: Function;
+    private _onStartArgs: [];
+    private _onUpdate: Function;
+    private _onUpdateArgs: [];
+    private _onComplete: Function;
+    private _onCompleteArgs: [];
+
+    constructor (target: Object, autoStart: boolean = true)
+    {
+        this.target = target;
+        this.autoStart = autoStart;
+        this._ease = Tween.defaultEasing;
+    }
+
+    static init (world: IBaseWorld): void
+    {
+        this.world = world;
+
+        this._updateListener = On(world.scene, 'update', (delta: number, time: number) => this.updateTweens(delta, time));
+    }
+
+    static killAllTweens (): void
+    {
+    }
+
+    static killTweensOf (target: unknown): void
+    {
+    }
+
+    static pauseAllTweens (): void
+    {
+    }
+
+    static resumeAllTweens (): void
+    {
+    }
+
+    static updateTweens (delta: number, time: number): void
+    {
+        if (!this.head)
+        {
+            return;
+        }
+
+        const complete: CompleteData[] = [];
+        let ct: number = 0;
+        let t: Tween = this.head;
+        let cpt: number = 0;
+
+        while (t)
+        {
+            cpt++;
+
+            let isComplete: boolean;
+
+            if (t.isDead)
+            {
+                isComplete = true
+            }
+            else
+            {
+                isComplete = time >= t.endTime;
+
+                let k = (isComplete) ? 1.0 : (time - t.startTime) / t._duration;
+                let ke = t._ease(k || 0);
+                let target = t.target;
+
+                //  update
+                let p: TweenProperty = t.properties;
+
+                while (p)
+                {
+                    target[p.name] = p.start + p.delta * ke;
+                    p = p.next;
+                }
+            }
+
+            if (isComplete)
+            {
+                if (t._started)
+                {
+                    let cd = new CompleteData(t._onComplete, t._onCompleteArgs, t._chain, t.endTime - time);
+                    t._chain = null;
+                    complete.unshift(cd);
+                    ct++;
+                }
+
+                t.isDead = true;
+                t.detach();
+                t.dispose();
+
+                let dead: Tween = t;
+                let prev: Tween = t.prev;
+                t = dead.next;
+
+                if (prev)
+                {
+                    prev.next = t;
+
+                    if (t)
+                    {
+                        t.prev = prev;
+                    }
+                }
+                else
+                {
+                    this.head = t;
+
+                    if (t)
+                    {
+                        t.prev = null;
+                    }
+                }
+
+                dead.prev = dead.next = null;
+            }
+            else
+            {
+                t = t.next;
+            }
+        }
+
+        if (ct)
+        {
+            for (let i = 0; i < ct; i++)
+            {
+                complete[i].execute();
+            }
+        }
+
+        this.tweenCount = cpt;
+    }
+
+    kill (setEndValues: boolean = false): void
+    {
+        if (this.isDead)
+        {
+            return;
+        }
+
+        if (setEndValues)
+        {
+            this._onUpdate = null;
+            this._onComplete = null;
+
+            this.update(this.endTime);
+        }
+        else
+        {
+            this.detach();
+            this.dispose();
+        }
+
+        this.isDead = true;
+    }
+
+    killTweens (): this
+    {
+        Tween.killTweensOf(this.target);
+
+        return this;
+    }
+
+    updateNow (): this
+    {
+        if (this._started)
+        {
+            let t: number = Math.max(this.startTime, performance.now());
+
+            this.update(t);
+        }
+        else
+        {
+            this.init();
+            this.endTime = this._duration = 1;
+            this.update(0);
+        }
+
+        return this;
+    }
+
+    private configure (duration: number | string, newState: Object = {}, reversed: boolean = false): void
+    {
+        this._configured = true;
+        this.reversed = reversed;
+        this.duration = duration;
+
+        for (let name in newState)
+        {
+            let value = newState[name];
+
+            // if (name in specialProperties)
+            // {
+            //     if (name == "alpha") { autoVisible = true; slowTween = true; }
+            //     else if (name == "alphaVisible") { name = "alpha"; autoVisible = false; }
+            //     else if (!(name in target))
+            //     {
+            //         if (name == "scale")
+            //         {
+            //             configure(duration, { scaleX:value, scaleY:value }, reversed);
+            //             continue;
+            //         }
+            //         else
+            //         {
+            //             specials = new specialProperties[name](target, name, value, specials);
+            //             slowTween = true;
+            //             continue;
+            //         }
+            //     }
+            // }
+
+            // if (value is Array && target[name] is Number)
+            // {
+            //     if ("__bezier" in specialProperties)
+            //     {
+            //         specials = new specialProperties["__bezier"](target, name, value, specials);
+            //         slowTween = true;
+            //     }
+            //     continue;
+            // }
+
+            this.properties = new TweenProperty(name, value, this.properties);
+        }
+    }
+
+    start (killTargetTweens: boolean = true, timeOffset: number = 0): void
+    {
+        if (this._started) return;
+        if (!this._inited) this.init();
+        this.overwrite = killTargetTweens;
+
+        // add to main tween chain
+        this.startTime = performance.now() + timeOffset;
+        this._duration = (isNaN(this.duration) ? this.smartDuration(String(this.duration)) : this.duration) * 1000;
+        this.endTime = this.startTime + this._duration;
+
+        // set values
+        if (this.reversed || this._duration == 0) this.update(this.startTime);
+
+        // if (autoVisible && _duration > 0) target.visible = true;
+
+        this._started = true;
+
+        this.attach(this.overwrite);
+    }
+
+    /// Read target properties
+    private init(): void
+    {
+        if (this._inited) return;
+
+        // configure properties
+        let p: TweenProperty = this.properties;
+
+        while (p) { p.init(this.target, this.reversed); p = p.next; }
+
+        // var s:EazeSpecial = specials;
+        // while (s) { s.init(reversed); s = s.next; }
+
+        this._inited = true;
+    }
+
+    /// Resolve non numeric durations
+    private smartDuration (duration: string): number
+    {
+        if (duration in Tween.defaultDuration) return Tween.defaultDuration[duration];
+
+        // else if (duration === 'auto')
+        // {
+        //     // look for a special property willing to provide an optimal duration
+        //     var s:EazeSpecial = specials;
+        //     while (s)
+        //     {
+        //         if ("getPreferredDuration" in s) return s["getPreferredDuration"]();
+        //         s = s.next;
+        //     }
+        // }
+
+        return Tween.defaultDuration.normal;
+    }
+
+    /**
+     * Set easing method
+     * @param	f	Easing function(k:Number):Number
+     * @return	Tween reference
+     */
+    easing (f: Function): this
+    {
+        this._ease = f || Tween.defaultEasing;
+
+        return this;
+    }
+
+    /// Update this tween alone
+    private update (time: number): void
+    {
+        // make this tween the only tween to update
+        let h: Tween = Tween.head;
+
+        Tween.head = this;
+
+        Tween.updateTweens(0, time);
+
+        Tween.head = h;
+    }
+
+    private attach (overwrite: boolean): void
+    {
+        let parallel: Tween = null;
+
+        if (overwrite) Tween.killTweensOf(this.target);
+        else parallel = Tween.running.get(this.target);
+
+        if (parallel)
+        {
+            this.prev = parallel;
+            this.next = parallel.next;
+
+            if (this.next) this.next.prev = this;
+            parallel.next = this;
+            this.rnext = parallel;
+        }
+        else
+        {
+            if (Tween.head) Tween.head.prev = this;
+            this.next = Tween.head;
+            Tween.head = this;
+        }
+
+        // Tween.running[target] = this;
+        Tween.running.set(this.target, this);
+    }
+
+    private detach (): void
+    {
+        if (this.target && this._started)
+        {
+            let targetTweens: Tween = Tween.running.get(this.target);
+
+            if (targetTweens === this)
+            {
+                if (this.rnext) Tween.running.set(this.target, this.rnext);
+                else Tween.running.delete(this.target);
+            }
+            else if (targetTweens)
+            {
+                let prev: Tween = targetTweens;
+
+                targetTweens = targetTweens.rnext;
+
+                while (targetTweens)
+                {
+                    if (targetTweens === this)
+                    {
+                        prev.rnext = this.rnext;
+                        break;
+                    }
+
+                    this.prev = targetTweens;
+
+                    targetTweens = targetTweens.rnext;
+                }
+            }
+
+            this.rnext = null;
+        }
+    }
+
+    private dispose (): void
+    {
+        if (this._started)
+        {
+            this.target = null;
+            this._onComplete = null;
+            this._onCompleteArgs = null;
+
+            if (this._chain)
+            {
+                // for each(let tween: Tween in this._chain) tween.dispose();
+
+                this._chain.forEach(tween =>
+                {
+                    tween.dispose();
+                });
+
+                this._chain = null;
+            }
+        }
+
+        if (this.properties) { this.properties.dispose(); this.properties = null; }
+
+        this._ease = null;
+        this._onStart = null;
+        this._onStartArgs = null;
+
+        // if (slowTween)
+        // {
+        //     if (specials) { specials.dispose(); specials = null; }
+        //     autoVisible = false;
+        //     _onUpdate = null;
+        //     _onUpdateArgs = null;
+        // }
+    }
+
+    delay (duration: number | string, overwrite: boolean = true): Tween
+    {
+        return this.add(duration, null, overwrite);
+    }
+
+    apply (newState: Object = null, overwrite: boolean = true): Tween
+    {
+        return this.add(0, newState, overwrite);
+    }
+
+    to (duration: number | string, newState: Object = null, overwrite: boolean = true): Tween
+    {
+        return this.add(duration, newState, overwrite);
+    }
+
+    from (duration: number | string, fromState: Object = null, overwrite: boolean = true): Tween
+    {
+        return this.add(duration, fromState, overwrite, true);
+    }
+
+    private add (duration: number | string, state: Object, overwrite: boolean, reversed: boolean = false): Tween
+    {
+        if (this.isDead) return new Tween(this.target).add(duration, state, overwrite, reversed);
+
+        if (this._configured) return this.chain().add(duration, state, overwrite, reversed);
+
+        this.configure(duration, state, reversed);
+
+        if (this.autoStart) this.start(overwrite);
+
+        return this;
+    }
+
+    chain (target: Object = null): Tween
+    {
+        let tween: Tween = new Tween(target || this.target, false);
+
+        if (!this._chain) this._chain = [];
+
+        this._chain.push(tween);
+
+        return tween;
+    }
+
+    get isStarted (): boolean { return this._started; }
+
+    get isFinished (): boolean { return this.isDead; }
+}
+
+function tween (target: Object): Tween
+{
+    return new Tween(target);
+}
+
+class Demo extends Scene
+{
+    constructor ()
+    {
+        super();
+
+        const world = new StaticWorld(this);
+
+        Tween.init(world);
+
+        const loader = new Loader();
+
+        loader.setPath('/phaser4-examples/public/assets/');
+        // loader.setPath('/examples/public/assets/');
+
+        loader.add(ImageFile('logo', 'logo.png'));
+
+        loader.start().then(() => {
+
+            const block = new Sprite(400, 100, 'logo');
+
+            tween(block).to(3, { y: 500, rotation: 0.2 });
+
+            AddChildren(world, block);
+
+        });
+
+    }
+}
+
+export default function (): void
+{
+    new Game(
+        WebGLRenderer(),
+        Size(800, 600),
+        Parent('gameParent'),
+        BackgroundColor(0x2d2d2d),
+        Scenes(Demo)
+    );
+}
