b3fffe9f6a4e5fc07d2723c695d9725a1ce53086
Richard Davey <rich@photonstorm.com>
2020-05-26 11:45:07 +0100

    Update tween 2.ts

--- dev/tween 2.ts
+++ dev/tween 2.ts
@@ -30,8 +30,8 @@ class Demo extends Scene
 
         const loader = new Loader();
 
-        // loader.setPath('/phaser4-examples/public/assets/');
-        loader.setPath('/examples/public/assets/');
+        loader.setPath('/phaser4-examples/public/assets/');
+        // loader.setPath('/examples/public/assets/');
 
         loader.add(ImageFile('logo', 'logo.png'));
         loader.add(ImageFile('rocket', 'rocket.png'));
