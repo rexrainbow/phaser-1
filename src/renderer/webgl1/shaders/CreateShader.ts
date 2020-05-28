553a27bcbd7ed4baa10f12ad095ff1d54a612ddf
Richard Davey <rich@photonstorm.com>
2020-05-28 18:04:52 +0100

    Creates a shader from the given source

---
+++ src/renderer/webgl1/shaders/CreateShader.ts
@@ -0,0 +1,29 @@
+export function CreateShader (gl: WebGLRenderingContext, source: string, type: GLenum): WebGLShader | null
+{
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+
+    gl.compileShader(shader);
+
+    const status = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+    if (!status)
+    {
+        const info = gl.getShaderInfoLog(shader);
+
+        //  Adding line numbers idea borrowed from Gregg Tavares!
+        const sourceLines = source.split('\n').map((line, index) =>
+        {
+            return `${index}: ${line}`;
+        });
+
+        console.error(`Error compiling shader: ${info}`, sourceLines.join('\n'));
+
+        gl.deleteShader(shader);
+
+        return null;
+    }
+
+    return shader;
+}
