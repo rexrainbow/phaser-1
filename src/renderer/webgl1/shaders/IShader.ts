e71e81a6461ed1247b75031957fe374a38e2b382
Richard Davey <rich@photonstorm.com>
2020-05-29 12:11:31 +0100

    Update IShader.ts

--- src/renderer/webgl1/shaders/IShader.ts
+++ src/renderer/webgl1/shaders/IShader.ts
@@ -4,18 +4,21 @@ import { Texture } from '../../../textures/Texture';
 
 export interface IShader
 {
-    renderer: IWebGLRenderer;
-    program: WebGLProgram;
+    attributes: Object;
     buffer: IndexedBuffer;
     count: number;
-    prevCount: number;
-    texture: Texture;
     framebuffer: WebGLFramebuffer;
+    prevCount: number;
+    program: WebGLProgram;
+    renderer: IWebGLRenderer;
     renderToFBO: boolean;
-    create (fragmentShaderSource: string, vertexShaderSource: string): void;
+    texture: Texture;
+    uniforms: Object;
+
     bind (uProjectionMatrix: Float32Array, uCameraMatrix: Float32Array, uTexture?: number): boolean;
     bindBuffers (indexBuffer: WebGLBuffer, vertexBuffer: WebGLBuffer): void;
-    setUniforms (): boolean;
+    create (fragmentShaderSource: string, vertexShaderSource: string): void;
     draw (count: number): void;
     flush (): boolean;
+    setUniforms (): boolean;
 }
