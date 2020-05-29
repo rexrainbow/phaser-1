720b142c6b357b40a3f0ff4b6f5e65e47c9cb1d8
Richard Davey <rich@photonstorm.com>
2020-05-29 15:37:26 +0100

    Create DefaultAttributes.ts

---
+++ src/renderer/webgl1/shaders/DefaultAttributes.ts
@@ -0,0 +1,14 @@
+import * as GL_CONST from '../GL_CONST';
+
+// gl.vertexAttribPointer(attribs.aVertexPosition, 2, gl.FLOAT, false, stride, 0);     // size = 8
+// gl.vertexAttribPointer(attribs.aTextureCoord, 2, gl.FLOAT, false, stride, 8);       // size = 8, offset = position
+// gl.vertexAttribPointer(attribs.aTextureId, 1, gl.FLOAT, false, stride, 16);         // size = 4, offset = position + tex coord
+// gl.vertexAttribPointer(attribs.aTintColor, 4, gl.UNSIGNED_BYTE, true, stride, 20);  // size = 4, offset = position + tex coord + index
+
+export const DefaultAttributes =
+{
+    aVertexPosition: { size: 2, type: GL_CONST.FLOAT, normalized: false, offset: 0 },
+    aTextureCoord: { size: 2, type: GL_CONST.FLOAT, normalized: false, offset: 8 },
+    aTextureId: { size: 1, type: GL_CONST.FLOAT, normalized: false, offset: 16 },
+    aTintColor: { size: 4, type: GL_CONST.UNSIGNED_BYTE, normalized: true, offset: 20 }
+};
