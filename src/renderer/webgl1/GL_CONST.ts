6d9b2cf3996998a89a927d3544d814472c90133f
Richard Davey <rich@photonstorm.com>
2020-05-29 15:36:40 +0100

    Created GL_CONST so we can define attributes without an active GL context

---
+++ src/renderer/webgl1/GL_CONST.ts
@@ -0,0 +1,17 @@
+const BYTE           = 0x1400;
+const UNSIGNED_BYTE  = 0x1401;
+const SHORT          = 0x1402;
+const UNSIGNED_SHORT = 0x1403;
+const INT            = 0x1404;
+const UNSIGNED_INT   = 0x1405;
+const FLOAT          = 0x1406;
+
+export {
+    BYTE,
+    UNSIGNED_BYTE,
+    SHORT,
+    UNSIGNED_SHORT,
+    INT,
+    UNSIGNED_INT,
+    FLOAT
+};
