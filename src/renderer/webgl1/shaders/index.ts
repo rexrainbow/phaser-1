b6ccda353bdb7b65ce33ae1e5bb934c60c468942
Richard Davey <rich@photonstorm.com>
2020-05-29 12:08:22 +0100

    Exposed all of the new functions

--- src/renderer/webgl1/shaders/index.ts
+++ src/renderer/webgl1/shaders/index.ts
@@ -1,11 +1,23 @@
 import { CheckShaderMaxIfStatements } from './CheckShaderMaxIfStatements';
+import { CreateProgram } from './CreateProgram';
+import { CreateShader } from './CreateShader';
+import { CreateUniformSetter } from './CreateUniformSetter';
+import { CreateUniforms } from './CreateUniforms';
+import { DeleteShaders } from './DeleteShaders';
 import { MultiTextureQuadShader } from './MultiTextureQuadShader';
 import { Shader } from './Shader';
+import { ShaderSystem } from './ShaderSystem';
 import { SingleTextureQuadShader } from './SingleTextureQuadShader';
 
 export {
     CheckShaderMaxIfStatements,
     MultiTextureQuadShader,
+    CreateProgram,
+    CreateShader,
+    CreateUniforms,
+    CreateUniformSetter,
+    DeleteShaders,
     Shader,
+    ShaderSystem,
     SingleTextureQuadShader
 };
