9b34f002681610b87abd6c45a54e456e3aaa8814
Richard Davey <rich@photonstorm.com>
2020-06-03 22:10:27 +0100

    Fixed render pass

--- src/renderer/webgl1/draw/DrawTexturedQuad.ts
+++ src/renderer/webgl1/draw/DrawTexturedQuad.ts
@@ -1,17 +1,11 @@
 import { BatchSingleQuad } from './BatchSingleQuad';
 import { IRenderPass } from './IRenderPass';
-import { IWebGLRenderer } from '../IWebGLRenderer';
 
 export function DrawTexturedQuad (renderPass: IRenderPass, x: number, y: number, width: number, height: number, u0: number, v0: number, u1: number, v1: number, textureIndex: number = 0, packedColor: number = 4294967295): void
 {
-    //  set the single quad shader
-    //  batch it
-    //  flush and reset to previous shader
+    renderPass.shader.setSingleQuadShader(textureIndex);
 
-    //  set single quad shader, not default:
-    // renderer.shaders.setDefault(textureIndex);
+    BatchSingleQuad(renderPass, x, y, width, height, u0, v0, u1, v1, textureIndex, packedColor);
 
-    // BatchSingleQuad(renderer, x, y, width, height, u0, v0, u1, v1, textureIndex, packedColor);
-
-    // renderer.shaders.popAndRebind();
+    renderPass.popShaderAndRebind();
 }
