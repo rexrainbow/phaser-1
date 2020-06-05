116bdc0784dc2a551fd10624bcfa662300c31723
Richard Davey <rich@photonstorm.com>
2020-06-05 03:33:11 +0100

    Let Bind worry about the comparison

--- src/renderer/webgl1/renderpass/SetViewport.ts
+++ src/renderer/webgl1/renderpass/SetViewport.ts
@@ -1,17 +1,12 @@
 import { AddViewport } from './AddViewport';
 import { BindViewport } from './BindViewport';
-import { Equals } from '../../../geom/rectangle/Equals';
 import { IRenderPass } from './IRenderPass';
 
 export function SetViewport (renderPass: IRenderPass, x: number = 0, y: number = 0, width: number = 0, height: number = 0): void
 {
     const entry = AddViewport(renderPass, x, y, width, height);
-    const currentViewport = renderPass.currentViewport;
 
-    if (currentViewport && !Equals(entry, currentViewport))
-    {
-        BindViewport(renderPass, entry);
-    }
+    BindViewport(renderPass, entry);
 
     renderPass.currentViewport = entry;
 }
