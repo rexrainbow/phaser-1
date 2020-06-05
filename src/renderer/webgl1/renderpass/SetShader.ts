6eb7348d568bb245cbba24af49f42b7491b3eef1
Richard Davey <rich@photonstorm.com>
2020-06-05 03:33:55 +0100

    No need to return bool + set current

--- src/renderer/webgl1/renderpass/SetShader.ts
+++ src/renderer/webgl1/renderpass/SetShader.ts
@@ -3,9 +3,11 @@ import { BindShader } from './BindShader';
 import { IRenderPass } from './IRenderPass';
 import { IShader } from '../shaders/IShader';
 
-export function SetShader (renderPass: IRenderPass, shader: IShader, textureID?: number, projectionMatrix?: Float32Array, cameraMatrix?: Float32Array): boolean
+export function SetShader (renderPass: IRenderPass, shader: IShader, textureID?: number, projectionMatrix?: Float32Array, cameraMatrix?: Float32Array): void
 {
     const entry = AddShader(renderPass, shader, textureID);
 
-    return BindShader(renderPass, projectionMatrix, cameraMatrix, entry);
+    BindShader(renderPass, projectionMatrix, cameraMatrix, entry);
+
+    renderPass.currentShader = entry;
 }
