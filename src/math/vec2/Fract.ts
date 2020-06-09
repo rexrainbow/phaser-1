64b8dee2bdc619bdd6d7e17152620b12d1a99408
Richard Davey <rich@photonstorm.com>
2020-06-09 13:59:58 +0100

    Update Fract.ts

--- src/math/vec2/Fract.ts
+++ src/math/vec2/Fract.ts
@@ -1,6 +1,8 @@
 import { IVec2 } from './IVec2';
 import { Vec2 } from './Vec2';
 
+//  Get a Vec2 from a Vec2s floored values
+
 export function Fract (a: IVec2, out: Vec2 = new Vec2()): IVec2
 {
     return out.set(
