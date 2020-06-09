be1517b0f75a6a886011eefada6d53bd47c59eb4
Richard Davey <rich@photonstorm.com>
2020-06-09 23:40:45 +0100

    Update RotationFromAxis.ts

--- src/math/vec3/RotationFromAxis.ts
+++ src/math/vec3/RotationFromAxis.ts
@@ -6,7 +6,7 @@ import { Vec3 } from './Vec3';
 export function RotationFromAxis (axis1: IVec3, axis2: IVec3, axis3: IVec3, out: Vec3 = new Vec3()): IVec3
 {
     //  zero out
-    out.set(0, 0, 0);
+    return out.set(0, 0, 0);
 
     //  TODO - Add ones quats are in
 }
