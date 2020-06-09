9c485ef9d8d96e57883d6b0284668bc9e11e6220
Richard Davey <rich@photonstorm.com>
2020-06-09 12:55:08 +0100

    Fixed Random

--- src/math/vec3/Random.ts
+++ src/math/vec3/Random.ts
@@ -4,9 +4,12 @@ import { Vec3 } from './Vec3';
 export function Random (a: IVec3, scale: number = 1, out: Vec3 = new Vec3()): IVec3
 {
     const r = Math.random() * 2 * Math.PI;
+    const z = Math.random() * 2 - 1;
+    const zScale = Math.sqrt(1 - z * z) * scale;
 
     return out.set(
-        Math.cos(r) * scale,
-        Math.sin(r) * scale
+        Math.cos(r) * zScale,
+        Math.sin(r) * zScale,
+        z * scale
     );
 }
