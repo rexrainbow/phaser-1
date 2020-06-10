8c5860ec91faf5ba34091c1bf72d530bfcd8a254
Richard Davey <rich@photonstorm.com>
2020-06-10 18:02:57 +0100

    Removed CreateVertexBuffer

--- src/renderer/webgl1/buffers/index.ts
+++ src/renderer/webgl1/buffers/index.ts
@@ -1,10 +1,8 @@
-import { CreateVertexBuffer } from './CreateVertexBuffer';
 import { DeleteGLBuffer } from './DeleteGLBuffer';
 import { IndexedVertexBuffer } from './IndexedVertexBuffer';
 import { VertexBuffer } from './VertexBuffer';
 
 export {
-    CreateVertexBuffer,
     DeleteGLBuffer,
     IndexedVertexBuffer,
     VertexBuffer
