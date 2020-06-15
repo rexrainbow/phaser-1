ab9de9c50cf2b724a52047ddca1dcb1b3b7f7683
Richard Davey <rich@photonstorm.com>
2020-06-15 18:01:43 +0100

    Flagged optional

--- src/math/vec3/IVec3.ts
+++ src/math/vec3/IVec3.ts
@@ -4,7 +4,7 @@ export interface IVec3
     y: number;
     z: number;
     set (x?: number, y?: number, z?: number): this;
-    toArray (dst: Float32List, index: number): Float32List;
-    fromArray (src: Float32List, index: number): this;
+    toArray (dst: Float32List, index?: number): Float32List;
+    fromArray (src: Float32List, index?: number): this;
     toString (): string;
 }
