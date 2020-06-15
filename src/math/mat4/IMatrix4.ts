727dba8ca8560a117454c42a3df8dea8b0780b4c
Richard Davey <rich@photonstorm.com>
2020-06-15 14:53:32 +0100

    Fixed component names

--- src/math/mat4/IMatrix4.ts
+++ src/math/mat4/IMatrix4.ts
@@ -1,7 +1,7 @@
 export interface IMatrix4
 {
     data: Float32Array;
-    set (m11: number, m12: number, m13: number, m14: number, m21: number, m22: number, m23: number, m24: number, m31: number, m32: number, m33: number, m34: number, m41: number, m42: number, m43: number, m44: number): this;
+    set (m00: number, m01: number, m02: number, m03: number, m10: number, m11: number, m12: number, m13: number, m20: number, m21: number, m22: number, m23: number, m30: number, m31: number, m32: number, m33: number): this;
     toArray (dst: Float32List, index?: number): Float32List;
     fromArray (src: Float32List, index?: number): this;
 }
