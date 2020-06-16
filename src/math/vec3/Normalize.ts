7b54d37b7b1c5d6e96d3264228cd7fee6052a493
Richard Davey <rich@photonstorm.com>
2020-06-16 14:22:01 +0100

    Update Normalize.ts

--- src/math/vec3/Normalize.ts
+++ src/math/vec3/Normalize.ts
@@ -1,9 +1,20 @@
-import { DivideScalar } from './DivideScalar';
 import { IVec3 } from './IVec3';
-import { Length } from './Length';
 import { Vec3 } from './Vec3';
 
 export function Normalize (a: IVec3, out: Vec3 = new Vec3()): IVec3
 {
-    return DivideScalar(a, Length(a) || 1, out);
+    const { x, y, z } = a;
+
+    let len = x * x + y * y + z * z;
+
+    if (len > 0)
+    {
+        len = 1 / Math.sqrt(len);
+    }
+
+    return out.set(
+        x * len,
+        y * len,
+        z * len
+    );
 }
