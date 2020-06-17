f46d4d0848fea5ea517444085559849620e3d5a1
Richard Davey <rich@photonstorm.com>
2020-06-17 17:38:26 +0100

    Added VecLike interfaces

---
+++ src/math/vec2/IVec2Like.ts
@@ -0,0 +1,5 @@
+export interface IVec2Like
+{
+    x: number;
+    y: number;
+}
--- src/math/vec4/IVec4Like.ts
+++ src/math/vec4/IVec4Like.ts
@@ -1,6 +1,7 @@
-export interface IVec3Like
+export interface IVec4Like
 {
     x: number;
     y: number;
     z: number;
+    w: number;
 }
