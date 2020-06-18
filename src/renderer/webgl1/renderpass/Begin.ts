0a4f984825257d39d703a5f01abca2751e7441aa
Richard Davey <rich@photonstorm.com>
2020-06-18 18:07:25 +0100

    Begin just sets the shader now, Start has done the grunt work

--- src/renderer/webgl1/renderpass/Begin.ts
+++ src/renderer/webgl1/renderpass/Begin.ts
@@ -1,24 +1,11 @@
-import { BindBlendMode } from './BindBlendMode';
-import { BindFramebuffer } from './BindFramebuffer';
 import { BindShader } from './BindShader';
-import { BindVertexBuffer } from './BindVertexBuffer';
-import { BindViewport } from './BindViewport';
 import { IBaseCamera } from '../../../camera/IBaseCamera';
-import { IMatrix4 } from '../../../math/mat4/IMatrix4';
 import { IRenderPass } from './IRenderPass';
 
-export function Begin (renderPass: IRenderPass, projectionMatrix: IMatrix4, camera2D: IBaseCamera): void
+export function Begin (renderPass: IRenderPass, camera2D: IBaseCamera): void
 {
-    renderPass.projectionMatrix = projectionMatrix;
     renderPass.current2DCamera = camera2D;
     renderPass.cameraMatrix = camera2D.matrix;
 
-    renderPass.count = 0;
-    renderPass.flushTotal = 0;
-
-    BindFramebuffer(renderPass, false);
-    BindBlendMode(renderPass);
-    BindViewport(renderPass);
-    BindVertexBuffer(renderPass);
     BindShader(renderPass);
 }
