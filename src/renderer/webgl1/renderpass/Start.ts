894f1bec0ee822270bb069bac0d831912846fdbd
Richard Davey <rich@photonstorm.com>
2020-06-18 18:07:13 +0100

    The new Start RenderPass function

---
+++ src/renderer/webgl1/renderpass/Start.ts
@@ -0,0 +1,21 @@
+import { BindBlendMode } from './BindBlendMode';
+import { BindFramebuffer } from './BindFramebuffer';
+import { BindVertexBuffer } from './BindVertexBuffer';
+import { BindViewport } from './BindViewport';
+import { IMatrix4 } from '../../../math/mat4/IMatrix4';
+import { IRenderPass } from './IRenderPass';
+
+export function Start (renderPass: IRenderPass, projectionMatrix: IMatrix4): void
+{
+    renderPass.projectionMatrix = projectionMatrix;
+    renderPass.current2DCamera = null;
+    renderPass.cameraMatrix = null;
+
+    renderPass.count = 0;
+    renderPass.flushTotal = 0;
+
+    BindFramebuffer(renderPass, false, renderPass.defaultFramebuffer);
+    BindBlendMode(renderPass, renderPass.defaultBlendMode);
+    BindViewport(renderPass, renderPass.defaultViewport);
+    BindVertexBuffer(renderPass, renderPass.defaultVertexBuffer);
+}
