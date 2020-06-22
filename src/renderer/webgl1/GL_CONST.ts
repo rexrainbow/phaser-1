bda0e60e29752cfe35e210bef84bd54f5695fa63
Richard Davey <rich@photonstorm.com>
2020-06-22 16:47:16 +0100

    Added some more useful consts

--- src/renderer/webgl1/GL_CONST.ts
+++ src/renderer/webgl1/GL_CONST.ts
@@ -1,10 +1,19 @@
-const BYTE           = 0x1400;
-const UNSIGNED_BYTE  = 0x1401;
-const SHORT          = 0x1402;
-const UNSIGNED_SHORT = 0x1403;
-const INT            = 0x1404;
-const UNSIGNED_INT   = 0x1405;
-const FLOAT          = 0x1406;
+const BYTE                   = 0x1400;
+const UNSIGNED_BYTE          = 0x1401;
+const SHORT                  = 0x1402;
+const UNSIGNED_SHORT         = 0x1403;
+const INT                    = 0x1404;
+const UNSIGNED_INT           = 0x1405;
+const FLOAT                  = 0x1406;
+const REPEAT                 = 0x2901;  // eslint-disable-line
+const CLAMP_TO_EDGE          = 0x812F;
+const MIRRORED_REPEAT        = 0x8370;  // eslint-disable-line
+const NEAREST                = 0x2600;  // eslint-disable-line
+const LINEAR                 = 0x2601;
+const NEAREST_MIPMAP_NEAREST = 0x2700;  // eslint-disable-line
+const LINEAR_MIPMAP_NEAREST  = 0x2701;  // eslint-disable-line
+const NEAREST_MIPMAP_LINEAR  = 0x2702;  // eslint-disable-line
+const LINEAR_MIPMAP_LINEAR   = 0x2703;  // eslint-disable-line
 
 export {
     BYTE,
@@ -13,5 +22,14 @@ export {
     UNSIGNED_SHORT,
     INT,
     UNSIGNED_INT,
-    FLOAT
+    FLOAT,
+    REPEAT,
+    CLAMP_TO_EDGE,
+    MIRRORED_REPEAT,
+    NEAREST,
+    LINEAR,
+    NEAREST_MIPMAP_NEAREST,
+    LINEAR_MIPMAP_NEAREST,
+    NEAREST_MIPMAP_LINEAR,
+    LINEAR_MIPMAP_LINEAR
 };
