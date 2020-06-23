d382177f75f1de351ad0bab9b9863e52a08a8ae1
Richard Davey <rich@photonstorm.com>
2020-06-23 14:21:16 +0100

    Tidying up

--- src/gameobjects3d/box/Box.ts
+++ src/gameobjects3d/box/Box.ts
@@ -1,7 +1,4 @@
 import { CreateBox } from '../../primitives/CreateBox';
-import { GetFacesFromVertexSet } from '../../primitives/faces/GetFacesFromVertexSet';
-import { IFace } from '../../primitives/faces/IFace';
-import { IRenderPass } from '../../renderer/webgl1/renderpass/IRenderPass';
 import { Mesh } from '../mesh/Mesh';
 
 export class Box extends Mesh
@@ -12,24 +9,4 @@ export class Box extends Mesh
 
         super(x, y, z, data);
     }
-
-    /*
-    renderGL <T extends IRenderPass> (renderPass: T): void
-    {
-        const buffer = renderPass.currentVertexBuffer;
-
-        const F32 = buffer.vertexViewF32;
-        const U32 = buffer.vertexViewU32;
-
-        //  TODO - If dirty transform, cache face data to local buffer then copy that to vbo
-        this.faces.forEach(face =>
-        {
-            face.addToBuffer(F32, U32, 1, buffer.offset);
-
-            buffer.add(3);
-
-            renderPass.count += 3;
-        });
-    }
-    */
 }
