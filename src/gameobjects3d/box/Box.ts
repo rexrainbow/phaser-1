7d2a06e66f59ba3ca3ef36342bdec2513de2e7be
Richard Davey <rich@photonstorm.com>
2020-06-23 18:12:38 +0100

    Default to 0x0x0

--- src/gameobjects3d/box/Box.ts
+++ src/gameobjects3d/box/Box.ts
@@ -5,7 +5,7 @@ export class Box extends Mesh
 {
     constructor (x: number = 0, y: number = 0, z: number = 0, width: number = 1, height: number = 1, depth: number = 1, widthSegments: number = 1, heightSegments: number = 1, depthSegments: number = 1)
     {
-        const data = CreateBox(x, y, z, width, height, depth, widthSegments, heightSegments, depthSegments);
+        const data = CreateBox(0, 0, 0, width, height, depth, widthSegments, heightSegments, depthSegments);
 
         super(x, y, z, data);
     }
