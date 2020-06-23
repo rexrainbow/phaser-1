b30eab15d8c594180a4aee7c1dac1c199c21e4d3
Richard Davey <rich@photonstorm.com>
2020-06-23 14:23:36 +0100

    Removed attribs, added uModelMatrix and tidied up class

--- src/renderer/webgl1/shaders/GoraudLambertShader.ts
+++ src/renderer/webgl1/shaders/GoraudLambertShader.ts
@@ -1,10 +1,8 @@
 import * as GL_CONST from '../GL_CONST';
 
+import { GORAUD_LAMBERT_FRAG } from '../glsl/GORAUD_LAMBERT_FRAG';
 import { GORAUD_LAMBERT_VERT } from '../glsl/GORAUD_LAMBERT_VERT';
-import { GetMaxTextures } from '../../../config/MaxTextures';
-import { IRenderPass } from '../renderpass/IRenderPass';
 import { IShader } from './IShader';
-import { MULTI_QUAD_FRAG } from '../glsl/MULTI_QUAD_FRAG';
 import { Shader } from './Shader';
 
 export class GoraudLambertShader extends Shader implements IShader
@@ -14,19 +12,18 @@ export class GoraudLambertShader extends Shader implements IShader
         super();
 
         const config = {
-            fragmentShader: MULTI_QUAD_FRAG,
+            fragmentShader: GORAUD_LAMBERT_FRAG,
             vertexShader: GORAUD_LAMBERT_VERT,
             attributes: {
                 aVertexPosition: { size: 3, type: GL_CONST.FLOAT, normalized: false, offset: 0 },
                 aVertexNormal: { size: 3, type: GL_CONST.FLOAT, normalized: false, offset: 12 },
-                aTextureCoord: { size: 2, type: GL_CONST.FLOAT, normalized: false, offset: 24 },
-                aTextureId: { size: 1, type: GL_CONST.FLOAT, normalized: false, offset: 32 },
-                aVertexColor: { size: 4, type: GL_CONST.UNSIGNED_BYTE, normalized: true, offset: 36 }
+                aTextureCoord: { size: 2, type: GL_CONST.FLOAT, normalized: false, offset: 24 }
             },
             uniforms: {
-                uProjectionMatrix: new Float32Array(),
-                uCameraMatrix: new Float32Array(),
-                uNormalMatrix: new Float32Array(),
+                uProjectionMatrix: new Float32Array(16),
+                uCameraMatrix: new Float32Array(16),
+                uNormalMatrix: new Float32Array(16),
+                uModelMatrix: new Float32Array(16).fill(0),
 
                 uTexture: 0,
                 uShininess: 10.10,
@@ -43,41 +40,5 @@ export class GoraudLambertShader extends Shader implements IShader
         };
 
         this.fromConfig(config);
-    }
-
-    create (fragmentShaderSource: string, vertexShaderSource: string, uniforms: {}, attribs: {}): void
-    {
-        const maxTextures = GetMaxTextures();
-
-        let src = '';
-
-        for (let i = 1; i < maxTextures; i++)
-        {
-            if (i > 1)
-            {
-                src += '\n\telse ';
-            }
-
-            if (i < maxTextures - 1)
-            {
-                src += `if (vTextureId < ${i}.5)`;
-            }
-
-            src += '\n\t{';
-            src += `\n\t\tcolor = texture2D(uTexture[${i}], vTextureCoord);`;
-            src += '\n\t}';
-        }
-
-        fragmentShaderSource = fragmentShaderSource.replace(/%count%/gi, `${maxTextures}`);
-        fragmentShaderSource = fragmentShaderSource.replace(/%forloop%/gi, src);
-
-        super.create(fragmentShaderSource, vertexShaderSource, uniforms, attribs);
-    }
-
-    bind (renderPass: IRenderPass): boolean
-    {
-        this.uniforms.set('uTexture', renderPass.textureIndex);
-
-        return super.bind(renderPass);
     }
 }
