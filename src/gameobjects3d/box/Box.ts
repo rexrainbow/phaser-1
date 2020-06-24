603d49314668b378189815cbcfaac8b5de459c07
Richard Davey <rich@photonstorm.com>
2020-06-24 17:48:47 +0100

    Creates geometry instance

--- src/gameobjects3d/box/Box.ts
+++ src/gameobjects3d/box/Box.ts
@@ -1,12 +1,15 @@
-import { CreateBox } from '../../primitives/CreateBox';
+import { BoxGeometry } from '../../geom3d/BoxGeometry';
+import { Geometry } from '../geometry/Geometry';
 import { Mesh } from '../mesh/Mesh';
 
 export class Box extends Mesh
 {
     constructor (x: number = 0, y: number = 0, z: number = 0, width: number = 1, height: number = 1, depth: number = 1, widthSegments: number = 1, heightSegments: number = 1, depthSegments: number = 1)
     {
-        const data = CreateBox(0, 0, 0, width, height, depth, widthSegments, heightSegments, depthSegments);
+        const data = BoxGeometry(0, 0, 0, width, height, depth, widthSegments, heightSegments, depthSegments);
 
-        super(x, y, z, data);
+        const geometry = new Geometry(data);
+
+        super(x, y, z, geometry);
     }
 }
