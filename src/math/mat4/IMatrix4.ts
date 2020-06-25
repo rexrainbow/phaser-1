1ccb49960c33f1cf952d532d24e5a73ad0645659
Richard Davey <rich@photonstorm.com>
2020-06-25 18:06:02 +0100

    Added destroy

--- src/math/mat4/IMatrix4.ts
+++ src/math/mat4/IMatrix4.ts
@@ -5,4 +5,5 @@ export interface IMatrix4
     set (m00: number, m01: number, m02: number, m03: number, m10: number, m11: number, m12: number, m13: number, m20: number, m21: number, m22: number, m23: number, m30: number, m31: number, m32: number, m33: number): this;
     toArray (dst: Float32List, index?: number): Float32List;
     fromArray (src: Float32List, index?: number): this;
+    destroy (): void;
 }
