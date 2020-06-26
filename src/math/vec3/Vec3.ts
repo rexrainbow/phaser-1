77ca065a802b5890c1fe7f7e8bd0064afd428f02
Richard Davey <rich@photonstorm.com>
2020-06-26 12:59:26 +0100

    Added common methods

--- src/math/vec3/Vec3.ts
+++ src/math/vec3/Vec3.ts
@@ -1,3 +1,7 @@
+import { Add, Clone, Cross, Distance, Divide, Dot, Inverse, Length, LengthSquared, Multiply, Negate, Normalize, Scale, Subtract } from './';
+
+import { IVec3Like } from './IVec3Like';
+
 export class Vec3
 {
     x: number;
@@ -16,6 +20,108 @@ export class Vec3
         this.z = z;
 
         return this;
+    }
+
+    copy (v: IVec3Like): this
+    {
+        return this.set(v.x, v.y, v.z);
+    }
+
+    add (va: IVec3Like, vb?: IVec3Like): this
+    {
+        if (vb)
+        {
+            Add(va, vb, this);
+        }
+        else
+        {
+            Add(this, va, this);
+        }
+
+        return this;
+    }
+
+    subtract (va: IVec3Like, vb?: IVec3Like): this
+    {
+        if (vb)
+        {
+            Subtract(va, vb, this);
+        }
+        else
+        {
+            Subtract(this, va, this);
+        }
+
+        return this;
+    }
+
+    multiply (v: IVec3Like): this
+    {
+        return Multiply(this, v, this) as this;
+    }
+
+    divide (v: IVec3Like): this
+    {
+        return Divide(this, v, this) as this;
+    }
+
+    inverse (): this
+    {
+        return Inverse(this, this) as this;
+    }
+
+    distance (v: IVec3Like): number
+    {
+        return Distance(this, v);
+    }
+
+    negate (): this
+    {
+        return Negate(this, this) as this;
+    }
+
+    cross (va: IVec3Like, vb?: IVec3Like): this
+    {
+        if (vb)
+        {
+            Cross(va, vb, this);
+        }
+        else
+        {
+            Cross(this, va, this);
+        }
+
+        return this;
+    }
+
+    scale (scalar: number): this
+    {
+        return Scale(this, scalar, this) as this;
+    }
+
+    normalize (): this
+    {
+        return Normalize(this, this) as this;
+    }
+
+    dot (v: IVec3Like): number
+    {
+        return Dot(this, v);
+    }
+
+    clone (): Vec3
+    {
+        return Clone(this);
+    }
+
+    get length (): number
+    {
+        return Length(this);
+    }
+
+    get lengthSquared (): number
+    {
+        return LengthSquared(this);
     }
 
     toArray (dst: Float32List = [], index: number = 0): Float32List
