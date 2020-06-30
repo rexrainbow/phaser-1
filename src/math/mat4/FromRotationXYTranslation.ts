09c4e30402fec24c8ce3b593cbcef1305fbd3113
Richard Davey <rich@photonstorm.com>
2020-06-30 19:52:51 +0100

    Factoring away more redundant calculations

--- src/math/mat4/FromRotationXYTranslation.ts
+++ src/math/mat4/FromRotationXYTranslation.ts
@@ -3,24 +3,6 @@ import { Matrix4 } from './Matrix4';
 
 export function FromRotationXYTranslation (rotation: IVec3Like, position: IVec3Like, translateFirst: boolean = true, out: Matrix4 = new Matrix4()): Matrix4
 {
-    //  Identity matrix
-    const a00 = 1;
-    const a01 = 0;
-    const a02 = 0;
-    const a03 = 0;
-    const a10 = 0;
-    const a11 = 1;
-    const a12 = 0;
-    const a13 = 0;
-    const a20 = 0;
-    const a21 = 0;
-    const a22 = 1;
-    const a23 = 0;
-    let a30 = 0;
-    let a31 = 0;
-    let a32 = 0;
-    let a33 = 1;
-
     const { x, y, z } = position;
 
     const sx = Math.sin(rotation.x);
@@ -29,6 +11,11 @@ export function FromRotationXYTranslation (rotation: IVec3Like, position: IVec3L
     const sy = Math.sin(rotation.y);
     const cy = Math.cos(rotation.y);
 
+    let a30 = x;
+    let a31 = y;
+    let a32 = z;
+
+    /*
     if (translateFirst)
     {
         // a30 = x + 0 * y + 0 * z;
@@ -43,28 +30,29 @@ export function FromRotationXYTranslation (rotation: IVec3Like, position: IVec3L
         // a33 = 0 * x + 0 * y + 0 * z + 1;
         // a33 = 1;
     }
+    */
 
     //  Rotate X
 
     // const b10 = a10 * cx + a20 * sx;
     // const b10 = 0 * cx + 0 * sx;
-    const b10 = 0;
+    // const b10 = 0;
 
     // const b11 = a11 * cx + a21 * sx;
     // const b11 = 1 * cx + 0 * sx;
-    const b11 = cx;
+    // const b11 = cx;
 
     // const b12 = a12 * cx + a22 * sx;
     // const b12 = 0 * cx + 1 * sx;
-    const b12 = sx;
+    // const b12 = sx;
 
     // const b13 = a13 * cx + a23 * sx;
     // const b13 = 0 * cx + 0 * sx;
-    const b13 = 0;
+    // const b13 = 0;
 
     // const b20 = a20 * cx - a10 * sx;
     // const b20 = 0 * cx - 0 * sx;
-    const b20 = 0;
+    // const b20 = 0;
 
     // const b21 = a21 * cx - a11 * sx;
     // const b21 = 0 * cx - 1 * sx;
@@ -72,17 +60,17 @@ export function FromRotationXYTranslation (rotation: IVec3Like, position: IVec3L
 
     // const b22 = a22 * cx - a12 * sx;
     // const b22 = 1 * cx - 0 * sx;
-    const b22 = cx;
+    // const b22 = cx;
 
     // const b23 = a23 * cx - a13 * sx;
     // const b23 = 0 * cx - 0 * sx;
-    const b23 = 0;
+    // const b23 = 0;
 
     //  Rotate Y
 
     // const c00 = a00 * cy - b20 * sy;
     // const c00 = 1 * cy - 0 * sy;
-    const c00 = cy;
+    // const c00 = cy;
 
     // const c01 = a01 * cy - b21 * sy;
     // const c01 = 0 * cy - b21 * sy;
@@ -96,11 +84,11 @@ export function FromRotationXYTranslation (rotation: IVec3Like, position: IVec3L
     // const c03 = a03 * cy - b23 * sy;
     // const c03 = 0 * cy - b23 * sy;
     // const c03 = 0 * cy - 0 * sy;
-    const c03 = 0;
+    // const c03 = 0;
 
     // const c20 = a00 * sy + b20 * cy;
     // const c20 = 1 * sy + 0 * cy;
-    const c20 = sy;
+    // const c20 = sy;
 
     // const c21 = a01 * sy + b21 * cy;
     // const c21 = 0 * sy + b21 * cy;
@@ -114,33 +102,41 @@ export function FromRotationXYTranslation (rotation: IVec3Like, position: IVec3L
     // const c23 = a03 * sy + b23 * cy;
     // const c23 = 0 * sy + b23 * cy;
     // const c23 = 0 * sy + 0 * cy;
-    const c23 = 0;
+    // const c23 = 0;
 
     //  Translate
     if (!translateFirst)
     {
-        a30 = c00 * x + b10 * y + c20 * z + a30;
-        a31 = c01 * x + b11 * y + c21 * z + a31;
-        a32 = c02 * x + b12 * y + c22 * z + a32;
-        a33 = c03 * x + b13 * y + c23 * z + a33;
+        // a30 = c00 * x + b10 * y + c20 * z;
+        a30 = cy * x + 0 * y + sy * z;
+
+        // a31 = c01 * x + b11 * y + c21 * z;
+        a31 = c01 * x + cx * y + c21 * z;
+
+        // a32 = c02 * x + b12 * y + c22 * z;
+        a32 = c02 * x + sx * y + c22 * z;
+
+        // a33 = c03 * x + b13 * y + c23 * z + 1;
+        // a33 = 0 * x + 0 * y + 0 * z + 1;
+        // a33 = 1;
     }
 
     return out.set(
-        c00,
+        cy,
         c01,
         c02,
-        c03,
-        b10,
-        b11,
-        b12,
-        b13,
-        c20,
+        0,
+        0,
+        cx,
+        sx,
+        0,
+        sy,
         c21,
         c22,
-        c23,
+        0,
         a30,
         a31,
         a32,
-        a33
+        1
     );
 }
