c4e6719cc12e1240b6af5e5dda1f1c06cc920385
Richard Davey <rich@photonstorm.com>
2020-07-01 17:51:48 +0100

    Update ICamera3D.ts

--- src/camera3d/ICamera3D.ts
+++ src/camera3d/ICamera3D.ts
@@ -1,6 +1,7 @@
 import { Vec3, Vec3Callback } from '../math/vec3';
 
 import { Euler } from '../math/euler/Euler';
+import { IRectangle } from '../geom/rectangle/IRectangle';
 import { IRenderer } from '../renderer/IRenderer';
 import { IVec3Like } from '../math/vec3/IVec3Like';
 import { Matrix4 } from '../math/mat4/Matrix4';
@@ -9,32 +10,37 @@ import { Quaternion } from '../math/quaternion';
 export interface ICamera3D
 {
     type: string;
+    renderer: IRenderer;
 
     position: Vec3Callback;
-    // scale: Vec3;
     rotation: Quaternion;
-
-    yaw: number;
-    pitch: number;
-    roll: number;
 
     matrix: Matrix4;
     viewMatrix: Matrix4;
-    viewNormal: Matrix4;
     projectionMatrix: Matrix4;
+    viewProjectionMatrix: Matrix4;
 
     forward: Vec3;
     up: Vec3;
     right: Vec3;
 
+    start: Vec3;
+
+    yaw: number;
+    pitch: number;
+    roll: number;
+
     aspect: number;
 
-    dirtyRender: boolean;
     isOrbit: boolean;
-    start: Vec3;
+
+    dirtyRender: boolean;
+
     panRate: number;
     zoomRate: number;
     rotateRate: number;
+
+    viewport: IRectangle;
 
     fov: number;
     near: number;
@@ -48,8 +54,8 @@ export interface ICamera3D
     panY (amount: number): this;
     panZ (amount: number): this;
     update (): this;
-    lookAt (target: IVec3Like, invert: boolean): this;
-    targetTo (target: IVec3Like): this;
+    // lookAt (target: IVec3Like, invert: boolean): this;
+    // targetTo (target: IVec3Like): this;
     setAspectRatio (value?: number): this;
     updateProjectionMatrix (): this;
 }
