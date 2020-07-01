db62765e51ba731c8333547cefd65b2515f95ddf
Richard Davey <rich@photonstorm.com>
2020-07-01 18:12:06 +0100

    Swapped to Ambient shader and added light functions

--- src/world3d/World3D.ts
+++ src/world3d/World3D.ts
@@ -1,10 +1,10 @@
 import { Flush, PopShader, PopVertexBuffer, SetShader, SetVertexBuffer } from '../renderer/webgl1/renderpass';
-import { Invert, Matrix4, Transpose } from '../math/mat4';
+import { RGBCallback, Vec3Callback } from '../math/vec3';
 
+import { AmbientLightShader } from '../renderer/webgl1/shaders/AmbientLightShader';
 import { BaseWorld3D } from './BaseWorld3D';
 import { Camera3D } from '../camera3d/Camera3D';
 import { CreateWorld3DRenderData } from './CreateWorld3DRenderData';
-import { GoraudLambertShader } from '../renderer/webgl1/shaders/GoraudLambertShader';
 import { ICamera3D } from '../camera3d/ICamera3D';
 import { IRenderPass } from '../renderer/webgl1/renderpass/IRenderPass';
 import { IScene } from '../scenes/IScene';
@@ -21,7 +21,14 @@ export class World3D extends BaseWorld3D
     enableCameraCull: boolean = true;
 
     shader: IShader;
-    normalMatrix: Matrix4;
+
+    lightDirection: Vec3Callback;
+    lightColor: RGBCallback;
+    ambientColor: RGBCallback;
+
+    private _lightDirection = [ 0.5, 3, 4 ];
+    private _lightColor = [ 1, 1, 1 ];
+    private _ambientColor = [ 0.2, 0.2, 0.2 ];
 
     constructor (scene: IScene)
     {
@@ -32,10 +39,28 @@ export class World3D extends BaseWorld3D
         // this.camera = new Camera3D(0, 0, 4);
         this.camera = new NewCamera3D();
 
-        this.shader = new GoraudLambertShader();
-        this.normalMatrix = new Matrix4();
+        this.shader = new AmbientLightShader();
+
+        this.lightDirection = new Vec3Callback(v => this.updateLightDirection(v));
+        this.lightColor = new RGBCallback(v => this.updateLightColor(v));
+        this.ambientColor = new RGBCallback(v => this.updateAmbientColor(v));
 
         this.renderData = CreateWorld3DRenderData(this, this.camera);
+    }
+
+    private updateLightDirection (v: Vec3Callback): void
+    {
+        this._lightDirection = [ v.x, v.y, v.z ];
+    }
+
+    private updateLightColor (v: Vec3Callback): void
+    {
+        this._lightColor = [ v.x, v.y, v.z ];
+    }
+
+    private updateAmbientColor (v: Vec3Callback): void
+    {
+        this._ambientColor = [ v.x, v.y, v.z ];
     }
 
     renderGL <T extends IRenderPass> (renderPass: T): void
@@ -48,19 +73,10 @@ export class World3D extends BaseWorld3D
 
         const uniforms = shader.uniforms;
 
-        //  TODO - Only if camera dirty:
         uniforms.set('uViewProjectionMatrix', camera.viewProjectionMatrix.data);
-        // uniforms.set('uProjectionMatrix', camera.projectionMatrix.data);
-        // uniforms.set('uCameraMatrix', camera.viewMatrix.data);
-        // uniforms.set('uCameraMatrix', camera.matrix.data);
-
-        // const normalMatrix = this.normalMatrix;
-
-        // Invert(camera.viewMatrix, normalMatrix);
-        // Transpose(normalMatrix, normalMatrix);
-
-        // uniforms.set('uNormalMatrix', normalMatrix.data);
-        // uniforms.set('uNormalMatrix', camera.viewNormal.data);
+        uniforms.set('uLightDirection', this._lightDirection);
+        uniforms.set('uLightColor', this._lightColor);
+        uniforms.set('uLightAmbient', this._ambientColor);
 
         //  TODO - Use fbo anyway to avoid z-fighting with World2D?
         // SetFramebuffer(renderPass, this.framebuffer, true);
@@ -72,6 +88,8 @@ export class World3D extends BaseWorld3D
         // const textureIndex = SetTexture(renderPass, this.cubeTexture);
 
         gl.enable(gl.DEPTH_TEST);
+
+        //  TODO - This is a per Mesh property:
         // gl.enable(gl.CULL_FACE);
 
         this.renderList.forEach(entry =>
