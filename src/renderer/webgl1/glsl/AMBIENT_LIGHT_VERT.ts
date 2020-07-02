c7b72a1d8b21c83c7c46ea8e088379c39fba8cf1
Richard Davey <rich@photonstorm.com>
2020-07-02 03:59:56 +0100

    Added Material and Light uniforms (move to structs?)

--- src/renderer/webgl1/glsl/AMBIENT_LIGHT_VERT.ts
+++ src/renderer/webgl1/glsl/AMBIENT_LIGHT_VERT.ts
@@ -11,19 +11,17 @@ uniform mat4 uViewProjectionMatrix;
 uniform mat4 uModelMatrix;
 uniform mat4 uNormalMatrix;
 
-uniform vec3 uLightColor;
-uniform vec3 uLightDirection;
-
 varying vec2 vTextureCoord;
-varying float vN;
+varying vec3 vNormal;
+varying vec3 vPosition;
 
 void main(void)
 {
     vTextureCoord = aTextureCoord;
 
-    vec3 normal = normalize(vec3(uNormalMatrix * vec4(aVertexNormal, 1.0)));
+    vPosition = vec3(uModelMatrix * vec4(aVertexPosition, 1.0));
 
-    vN = max(dot(normalize(uLightDirection), normal), 0.0);
+    vNormal = vec3(uNormalMatrix * vec4(aVertexNormal, 1.0));
 
     gl_Position = uViewProjectionMatrix * uModelMatrix * vec4(aVertexPosition, 1.0);
 }
