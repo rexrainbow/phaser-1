c35a6d78043fac608bfce996229c9c9aa6b5e966
Richard Davey <rich@photonstorm.com>
2020-07-02 04:00:17 +0100

    Added Material

--- src/gameobjects3d/mesh/IMesh.ts
+++ src/gameobjects3d/mesh/IMesh.ts
@@ -1,6 +1,7 @@
 import { Frame } from '../../textures/Frame';
 import { Geometry } from '../geometry/Geometry';
 import { IGameObject3D } from '../IGameObject3D';
+import { Material } from '../material/Material';
 import { Texture } from '../../textures/Texture';
 
 export interface IMesh extends IGameObject3D
@@ -9,6 +10,8 @@ export interface IMesh extends IGameObject3D
     frame: Frame;
     hasTexture: boolean;
     geometry: Geometry;
+    material: Material;
     setTexture (key: string | Texture, frame?: string | number): this;
     setFrame (key?: string | number | Frame): this;
+    setMaterial (material: Material): this;
 }
--- src/gameobjects3d/mesh/Mesh.ts
+++ src/gameobjects3d/mesh/Mesh.ts
@@ -4,6 +4,7 @@ import { GameObject3D } from '../GameObject3D';
 import { Geometry } from '../geometry/Geometry';
 import { IGameObject3D } from '../IGameObject3D';
 import { IRenderPass } from '../../renderer/webgl1/renderpass/IRenderPass';
+import { Material } from '../material/Material';
 import { SetTexture as RequestTexture } from '../../renderer/webgl1/renderpass/SetTexture';
 import { SetFrame } from './SetFrame';
 import { SetTexture } from './SetTexture';
@@ -16,14 +17,16 @@ export class Mesh extends GameObject3D
     hasTexture: boolean = false;
 
     geometry: Geometry;
+    material: Material;
 
     cullFaces: boolean = true;
 
-    constructor (x: number = 0, y: number = 0, z: number = 0, geometry?: Geometry)
+    constructor (x: number = 0, y: number = 0, z: number = 0, geometry?: Geometry, material: Material = new Material())
     {
         super(x, y, z);
 
         this.geometry = geometry;
+        this.material = material;
     }
 
     setTexture (key: string | Texture, frame?: string | number): this
@@ -40,6 +43,13 @@ export class Mesh extends GameObject3D
         return this;
     }
 
+    setMaterial (material: Material): this
+    {
+        this.material = material;
+
+        return this;
+    }
+
     renderGL <T extends IRenderPass> (renderPass: T): void
     {
         const shader = renderPass.currentShader.shader;
@@ -48,7 +58,10 @@ export class Mesh extends GameObject3D
 
         shader.setUniform('uModelMatrix', this.transform.local.data);
         shader.setUniform('uNormalMatrix', this.transform.normal.data);
+
         shader.setUniform('uTexture', textureIndex);
+
+        this.material.setUniforms(shader);
 
         FlushBuffer(renderPass, this.geometry.buffer);
     }
@@ -57,7 +70,8 @@ export class Mesh extends GameObject3D
     {
         super.destroy(reparentChildren);
 
-        this.geometry.destroy();
+        this.geometry = null;
+        this.material = null;
         this.texture = null;
         this.frame = null;
         this.hasTexture = false;
